8492cef9415d48d9828ada1c8d72c56d97ef2b97
Matt Fletcher <github@ecartz.biz>
2021-03-22 14:07:21 +0000

    Update version check
    Minor code clean up, e.g. to eliminate unnecessary whitespace.
    
    Migrated from tep_get_version to new Versions class.
    
    Added condition for when the version feed server fails to answer or
    when the XML load is broken.

--- admin/includes/languages/english/version_check.php
+++ admin/includes/languages/english/version_check.php
@@ -18,5 +18,11 @@ const TABLE_HEADING_ACTION = 'Action';
 
 const TITLE_INSTALLED_VERSION = 'Installed Version: <strong>CE Phoenix v%s</strong>';
 
+const VERSION_SERVER_FAILURE = 'Failed to load the available versions from the server.  Please check your internet or try again later.';
 const VERSION_RUNNING_LATEST = 'You are running the latest version of CE Phoenix.';
-define('VERSION_UPGRADES_AVAILABLE', '<strong>CE Phoenix %s</strong> is the latest version available for you!<hr><a class="alert-link" target="_blank" href="https://phoenixcart.org/forum/viewtopic.php?f=26&t=309">Certified Developers</a> and detailed "<a class="alert-link" target="_blank" href="https://phoenixcart.org/forum/app.php/tag/phoenix-update">Do It Yourself</a>" instructions are available in the Phoenix Club.');
+const VERSION_UPGRADES_AVAILABLE = <<<'EOT'
+<strong>CE Phoenix %s</strong> is the latest version available for you!<hr>
+<a class="alert-link" target="_blank" href="https://phoenixcart.org/forum/viewtopic.php?f=26&t=309">Certified Developers</a>
+ and detailed "<a class="alert-link" target="_blank" href="https://phoenixcart.org/forum/app.php/tag/phoenix-update">Do It Yourself</a>"
+ instructions are available in the Phoenix Club.
+EOT;
