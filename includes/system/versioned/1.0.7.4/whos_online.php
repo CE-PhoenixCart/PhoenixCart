5c888f15c14bb3f7177888d27f041aaaff4b6ef2
Matt Fletcher <github@ecartz.biz>
2021-03-28 17:17:26 +0000

    Default session ID if not provided
    Allows update_session_id to be called without specifying the new
    session ID.  This makes it easier to be called by a hook.
    
    Update to the new Database methods.
    
    Both of these changes are backward compatible, so no new version is
    necessary.

--- includes/system/versioned/1.0.7.4/whos_online.php
+++ includes/system/versioned/1.0.7.4/whos_online.php
@@ -13,6 +13,8 @@
   class whos_online {
 
     public static function update() {
+      global $db;
+
       if (isset($_SESSION['customer_id'])) {
         $wo_customer_id = $_SESSION['customer_id'];
         $wo_full_name = $GLOBALS['customer']->get('name');
@@ -24,16 +26,16 @@
       $current_time = time();
       static::expire($current_time);
 
-      tep_db_query(<<<'EOSQL'
+      $db->query(<<<'EOSQL'
 INSERT INTO whos_online (customer_id, full_name, session_id, ip_address, time_entry, time_last_click, last_page_url) VALUES (
 EOSQL
         . (int)$wo_customer_id
-        . ", '" . tep_db_input($wo_full_name)
-        . "', '" . tep_db_input(session_id())
-        . "', '" . tep_db_input(tep_get_ip_address())
-        . "', '" . tep_db_input($current_time)
-        . "', '" . tep_db_input($current_time)
-        . "', '" . tep_db_input(tep_db_prepare_input(getenv('REQUEST_URI'))) . <<<'EOSQL'
+        . ", '" . $db->escape($wo_full_name)
+        . "', '" . $db->escape(session_id())
+        . "', '" . $db->escape(Request::get_ip())
+        . "', '" . $db->escape($current_time)
+        . "', '" . $db->escape($current_time)
+        . "', '" . $db->escape(Text::input(getenv('REQUEST_URI'))) . <<<'EOSQL'
 ') ON DUPLICATE KEY UPDATE
   customer_id = VALUES(customer_id),
   full_name = VALUES(full_name),
@@ -45,11 +47,13 @@ EOSQL
     }
 
     protected static function expire($current_time) {
-      tep_db_query("DELETE FROM whos_online WHERE time_last_click < " . (int)($current_time - 900));
+      $GLOBALS['db']->query("DELETE FROM whos_online WHERE time_last_click < " . (int)($current_time - 900));
     }
 
-    public static function update_session_id($old_id, $new_id) {
-      tep_db_query(sprintf(<<<'EOSQL'
+    public static function update_session_id($old_id, $new_id = null) {
+      global $db;
+
+      $db->query(sprintf(<<<'EOSQL'
 INSERT INTO whos_online (customer_id, full_name, session_id, ip_address, time_entry, time_last_click, last_page_url)
  SELECT wo.customer_id, wo.full_name, '%s', wo.ip_address, wo.time_entry, wo.time_last_click, wo.last_page_url
    FROM whos_online wo
@@ -62,8 +66,8 @@ INSERT INTO whos_online (customer_id, full_name, session_id, ip_address, time_en
    time_last_click = VALUES(time_last_click),
    last_page_url = VALUES(last_page_url)
 EOSQL
-        , tep_db_input($new_id), tep_db_input($old_id)));
-      tep_db_query("DELETE FROM whos_online WHERE session_id = '" . tep_db_input($old_id) . "' OR time_last_click < " . (int)(time() - 900));
+        , $db->escape($new_id ?? session_id()), $db->escape($old_id)));
+      $db->query("DELETE FROM whos_online WHERE session_id = '" . $db->escape($old_id) . "' OR time_last_click < " . (int)(time() - 900));
     }
 
   }
