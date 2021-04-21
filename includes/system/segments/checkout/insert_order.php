7d478812037d18c9385c2b91d1f1107cd9991b25
Matt Fletcher <github@ecartz.biz>
2021-04-21 19:10:31 +0000

    Fix order insertion of attributes

--- includes/system/segments/checkout/insert_order.php
+++ includes/system/segments/checkout/insert_order.php
@@ -97,6 +97,7 @@ SELECT popt.products_options_name, poval.products_options_values_name, pa.option
 EOSQL;
   }
 
+  $db =& $GLOBALS['db'];
   $sql_data['orders_products'] = [];
   $sql_data['orders_products_attributes'] = [];
   $sql_data['orders_products_download'] = [];
@@ -137,7 +138,6 @@ EOSQL;
 
   $parameters = [ 'sql_data' => &$sql_data ];
   $GLOBALS['hooks']->call('siteWide', 'insertOrder', $parameters);
-  $db =& $GLOBALS['db'];
 
   $db->perform('orders', $sql_data['orders']);
   $GLOBALS['order_id'] = mysqli_insert_id($db);
