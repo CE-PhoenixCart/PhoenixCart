f8952433fe0a64fd97f3568ea2739385a59b99b3
Matt Fletcher <github@ecartz.biz>
2021-06-07 05:14:53 +0000

    Use a new infobox component rather than the old box class
    To support the change from a switch statement to a collection of
    files, this switches from the old box class to a new infobox
    component.  This will be simpler than the original plan to migrate
    the switch into a box function attached to the table definition.
    Also, this allows for new actions to be added as simply as creating
    a file (or two if the action causes behavior both at the top and
    bottom of the file).
    
    This is also part of a general move away from using classes for
    display.  This is more like a MVC application, with the display in
    separate files from the data and processing.
    
    In the long term, this should allow the box and tableBlock classes
    to be deprecated.  Which is helpful because the two reinforce the
    table-based layout.  With this version, changing that layout would
    be as simple as replacing the component file.
    
    The component is also simpler than the tableBlock version, which
    is over-engineered with capabilities that are never used.  And of
    course it only uses one file where the other uses box, tableBlock,
    and a template file.

---
+++ admin/includes/components/infobox.php
@@ -0,0 +1,71 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  $heading = '';
+  $contents = [];
+  require $action_file;
+
+  if ( !Text::is_empty($heading) && ([] !== $contents) ) {
+    $parameters = [
+      'heading' => &$heading,
+      'contents' => &$contents,
+    ];
+    $GLOBALS['admin_hooks']->cat('infoBox', $parameters);
+
+    if (isset($contents['form'])) {
+      $form_start = $contents['form'] . PHP_EOL;
+      $form_close = '</form>' . PHP_EOL;
+      unset($contents['form']);
+    } else {
+      $form_start = '';
+      $form_close = '';
+    }
+?>
+    <div class="col-12 col-sm-4">
+      <div class="table-responsive">
+<?php
+    if ('' !== $form_start) {
+      echo $form_start;
+    }
+?>
+        <table class="table table-striped table-hover">
+          <thead class="thead-light">
+            <tr>
+              <th><?= $heading ?></th>
+            </tr>
+          </thead>
+          <tbody>
+<?php
+    foreach ($contents as $row) {
+      echo '<tr>' . PHP_EOL;
+
+      echo '<td';
+      if (isset($row['class']) && !Text::is_empty($row['class'])) {
+        echo ' class="' . $row['class'] . '"';
+      }
+      echo '>' . $row['text'] . '</td>' . PHP_EOL;
+      echo '</tr>' . PHP_EOL;
+    }
+?>
+
+          </tbody>
+        </table>
+<?php
+    if ('' !== $form_close) {
+      echo $form_close;
+    }
+?>
+      </div>
+    </div>
+<?php
+  }
+?>
