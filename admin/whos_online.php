1f356be309a60cb8982fb56ec0f7fe4031d4abcb
Matt Fletcher <104613+ecartz@users.noreply.github.com>
2021-10-02 13:29:01 +0000

    Modernize Who's online

--- admin/whos_online.php
+++ admin/whos_online.php
@@ -18,103 +18,71 @@
   $db->query("DELETE FROM whos_online WHERE time_last_click < " . (int)$xx_mins_ago);
 
   require 'includes/template_top.php';
+
+  $table_definition = [
+    'columns' => [
+      [
+        'name' => TABLE_HEADING_ONLINE,
+        'function' => function (&$row) {
+          return gmdate('H:i:s', time() - $row['time_entry']);
+        },
+      ],
+      [
+        'name' => TABLE_HEADING_CUSTOMER_ID,
+        'function' => function (&$row) {
+          return $row['customer_id'];
+        },
+      ],
+      [
+        'name' => TABLE_HEADING_FULL_NAME,
+        'function' => function (&$row) {
+          return $row['full_name'];
+        },
+      ],
+      [
+        'name' => TABLE_HEADING_IP_ADDRESS,
+        'function' => function (&$row) {
+          return $row['ip_address'];
+        },
+      ],
+      [
+        'name' => TABLE_HEADING_ENTRY_TIME,
+        'class' => 'text-right',
+        'function' => function ($row) {
+          return date('H:i:s', $row['time_entry']);
+        },
+      ],
+      [
+        'name' => TABLE_HEADING_LAST_CLICK,
+        'class' => 'text-right',
+        'function' => function (&$row) {
+          return date('H:i:s', $row['time_last_click']);
+        },
+      ],
+      [
+        'name' => TABLE_HEADING_LAST_PAGE_URL,
+        'class' => 'text-right',
+        'function' => function (&$row) {
+          return preg_replace('{ceid=[A-Z0-9,-]+[&]*}i', '', $row['last_page_url']);
+        },
+      ],
+    ],
+    'count_text' => TEXT_DISPLAY_NUMBER_OF_CUSTOMERS,
+    'page' => $_GET['page'] ?? null,
+    'web_id' => 'info',
+    'db_id' => 'session_id',
+    'rows_per_page' => MAX_DISPLAY_SEARCH_RESULTS,
+    'sql' => "SELECT * FROM whos_online",
+  ];
+
+  $table_definition['split'] = new Paginator($table_definition);
 ?>
 
   <h1 class="display-4 mb-2"><?= HEADING_TITLE ?></h1>
 
-  <div class="row no-gutters">
-    <div class="col-12 col-sm-8">
-      <div class="table-responsive">
-        <table class="table table-striped table-hover">
-          <thead class="thead-dark">
-            <tr>
-              <th><?= TABLE_HEADING_ONLINE ?></th>
-              <th><?= TABLE_HEADING_CUSTOMER_ID ?></th>
-              <th><?= TABLE_HEADING_FULL_NAME ?></th>
-              <th><?= TABLE_HEADING_IP_ADDRESS ?></th>
-              <th class="text-right"><?= TABLE_HEADING_ENTRY_TIME ?></th>
-              <th class="text-right"><?= TABLE_HEADING_LAST_CLICK ?></th>
-              <th class="text-right"><?= TABLE_HEADING_LAST_PAGE_URL ?></th>
-            </tr>
-          </thead>
-          <tbody>
-          <?php
-          $whos_online_query = $db->query("SELECT * FROM whos_online");
-          while ($whos_online = $whos_online_query->fetch_assoc()) {
-            $time_online = (time() - $whos_online['time_entry']);
-            if (!isset($info) && (!isset($_GET['info']) || ($_GET['info'] == $whos_online['session_id']))) {
-              $info = new ObjectInfo($whos_online);
-            }
-
-            if (isset($info->session_id) && ($whos_online['session_id'] == $info->session_id)) {
-              echo '<tr class="table-active">';
-            } else {
-              echo '<tr onclick="document.location.href=\'' . $Admin->link()->retain_query_except(['action'])->set_parameter('info', $whos_online['session_id']) . '\'">';
-            }
-?>
-                <td><?= gmdate('H:i:s', $time_online) ?></td>
-                <td><?= $whos_online['customer_id'] ?></td>
-                <td><?= $whos_online['full_name'] ?></td>
-                <td><?= $whos_online['ip_address'] ?></td>
-                <td class="text-right"><?= date('H:i:s', $whos_online['time_entry']) ?></td>
-                <td class="text-right"><?= date('H:i:s', $whos_online['time_last_click']) ?></td>
-                <td class="text-right"><?= preg_replace('/ceid=[A-Z0-9,-]+[&]*/i', '', $whos_online['last_page_url']) ?></td>
-              </tr>
 <?php
-  }
-?>
-          </tbody>
-        </table>
-      </div>
+  $table_definition['split']->display_table();
 
-      <p><?php printf(TEXT_NUMBER_OF_CUSTOMERS, mysqli_num_rows($whos_online_query)); ?></p>
-
-    </div>
-
-<?php
-  $heading = [];
-  $contents = [];
-
-  if (isset($info)) {
-    $heading[] = ['text' => TABLE_HEADING_SHOPPING_CART];
-
-    if ( $info->customer_id > 0 ) {
-      $session_customer_id = $_SESSION['customer_id'] ?? null;
-      $session_currency = $_SESSION['currency'] ?? null;
-      $_SESSION['customer_id'] = $info->customer_id;
-      $_SESSION['currency'] = DEFAULT_CURRENCY;
-
-      $shoppingCart = new shoppingCart();
-      $shoppingCart->restore_contents();
-
-      foreach ($shoppingCart->get_products() as $product) {
-        $contents[] = ['text' => sprintf(TEXT_SHOPPING_CART_ITEM, $product->get('quantity'), $product->get('name'))];
-      }
-
-      $currencies = new currencies();
-      $contents[] = [
-        'class' => 'table-dark text-right',
-        'text' => sprintf(TEXT_SHOPPING_CART_SUBTOTAL, $currencies->format($shoppingCart->show_total())),
-      ];
-
-      $_SESSION['customer_id'] = $session_customer_id;
-      $_SESSION['currency'] = $session_currency;
-    } else {
-      $contents[] = ['text' => TEXT_SHOPPING_CART_NA];
-    }
-  }
-
-  if ( ($heading !== []) && ($contents !== []) ) {
-    echo '<div class="col-12 col-sm-4">';
-      $box = new box();
-      echo $box->infoBox($heading, $contents);
-    echo '</div>';
-  }
-?>
-
-  </div>
-
-<?php
   require 'includes/template_bottom.php';
   require 'includes/application_bottom.php';
 ?>
