7af5dfdfbe8559887a0c6ed6739152865b024deb
Matt Fletcher <104613+ecartz@users.noreply.github.com>
2021-12-11 03:00:45 +0000

    Modernize

--- includes/classes/alert_block.php
+++ includes/classes/alert_block.php
@@ -9,29 +9,34 @@
 
   Released under the GNU General Public License
 */
-  class alertBlock {    
-	// class constructor
-  	function __construct() {
+
+  class alertBlock {
+
+  	public function __construct() {
   	}
 
-    function alertBlock($contents, $alert_output = false) {
+    public function alertBlock($alerts, $alert_output = false) {
 	  $alertBox_string = '';
-		  
-      for ($i=0, $n=sizeof($contents); $i<$n; $i++) {
+
+      foreach ($alerts as $alert) {
         $alertBox_string .= '  <div';
-		  
-        if (isset($contents[$i]['params']) && tep_not_null($contents[$i]['params']))
-		  $alertBox_string .= ' ' . $contents[$i]['params'];
-        
-		  $alertBox_string .= '>' . "\n";
-          $alertBox_string .= '	<button type="button" class="close" data-dismiss="alert">&times;</button>' . "\n";
-          $alertBox_string .= $contents[$i]['text'];
-    
-          $alertBox_string .= '  </div>' . "\n";
+
+        if (isset($alert['params']) && !Text::is_empty($alert['params'])) {
+          $alertBox_string .= ' ' . $alert['params'];
+        }
+
+        $alertBox_string .= '>' . "\n";
+        $alertBox_string .= '	<button type="button" class="close" data-dismiss="alert">&times;</button>' . "\n";
+        $alertBox_string .= $alert['text'];
+
+       $alertBox_string .= '  </div>' . "\n";
       }
 
-      if ($alert_output == true) echo $alertBox_string;
-        return $alertBox_string;
-     }
+      if ($alert_output) {
+        echo $alertBox_string;
+      }
+
+      return $alertBox_string;
+    }
+
   }
-?>
