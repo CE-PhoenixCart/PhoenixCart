7e1f95ce553709f5c092628c3f3d2341df619810
Matt Fletcher <104613+ecartz@users.noreply.github.com>
2022-01-20 11:21:20 +0000

    Tyop

--- includes/modules/content/footer/templates/tpl_cm_footer_contact_us.php
+++ includes/modules/content/footer/templates/tpl_cm_footer_contact_us.php
@@ -7,7 +7,7 @@
     <?= MODULE_CONTENT_FOOTER_CONTACT_US_EMAIL . STORE_OWNER_EMAIL_ADDRESS ?>
   </address>
   <ul class="list-unstyled">
-    <li><a class="btn btn-success btn=role="button" href="<?= $GLOBALS['Linker']->build('contact_us.php') ?>"><i class="fas fa-paper-plane"></i> <?= MODULE_CONTENT_FOOTER_CONTACT_US_EMAIL_LINK ?></a></li>
+    <li><a class="btn btn-success btn-block" role="button" href="<?= $GLOBALS['Linker']->build('contact_us.php') ?>"><i class="fas fa-paper-plane"></i> <?= MODULE_CONTENT_FOOTER_CONTACT_US_EMAIL_LINK ?></a></li>
   </ul>
 </div>
 
