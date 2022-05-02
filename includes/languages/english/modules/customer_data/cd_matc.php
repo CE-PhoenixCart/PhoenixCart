ed2c30b7cc98830e95585b51da19aa2e62c3f547
Matt Fletcher <104613+ecartz@users.noreply.github.com>
2022-05-02 07:01:09 +0000

    Migrate MATC to a customer data module
    The MATC is implemented as a hook.  This made sense when it was
    originally created, as customer data modules did not exist then and
    it was a separate module.  However, it is currently part of the
    default install, as it is require in GDPR jurisdictions.  But it is
    not required in non-GDPR jurisdictions.
    
    Migrating it to a customer data module makes it possible to turn it
    off and increases the configuration options.

---
+++ includes/languages/english/modules/customer_data/cd_matc.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+const MODULE_CUSTOMER_DATA_MATC_TEXT_TITLE = 'MATC';
+const MODULE_CUSTOMER_DATA_MATC_TEXT_DESCRIPTION = 'Require agreement with the terms and conditions.';
+
+// for the page(s)
+const ENTRY_MATC = 'Legal agreements';
+const ENTRY_MATC_TEXT = 'By clicking this box you agree to our <a href="#" role="button" class="card-link ml-0 border-bottom border-primary" data-toggle="modal" data-target="#TCModal">T&amp;C</a> and <a href="#" role="button" class="card-link ml-0 border-bottom border-primary" data-toggle="modal" data-target="#PModal">Privacy</a> policies.';
+
+// for the modal popup
+const MATC_BUTTON_CLOSE = 'Close';
---
+++ includes/modules/customer_data/templates/tpl_cd_matc.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+?>
+  <div class="form-group row align-items-center">
+    <div class="col-form-label col-sm-3 text-left text-sm-right"><?= ENTRY_MATC ?></div>
+    <div class="col-sm-9 pl-5 custom-control custom-switch">
+      <?= $input->set('id', 'inputMATC') ?>
+      <label for="inputMATC" class="custom-control-label text-muted"><small><?= ENTRY_MATC_TEXT ?></small></label>
+    </div>
+  </div>
