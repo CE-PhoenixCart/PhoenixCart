37ab9ea4ced86cd28d4957d99184f15cabe9fb9b
Matt Fletcher <104613+ecartz@users.noreply.github.com>
2022-07-22 00:25:55 +0000

    Modernize admin/configuration
    Updates the admin/configuration.php file to
    
    1.  Use separate action (including infobox) files.
    2.  Use the paginated table component.
    3.  Minor code tweaks for consistency with other code.

---
+++ admin/includes/actions/configuration/infoboxes/edit.php
@@ -0,0 +1,30 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2022 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  $cInfo = &$GLOBALS['table_definition']['info'];
+  $heading = $cInfo->configuration_title;
+  $GLOBALS['link']->set_parameter('cID', (int)$cInfo->configuration_id);
+
+  if ($cInfo->set_function) {
+    eval('$value_field = ' . $cInfo->set_function . '"' . htmlspecialchars($cInfo->configuration_value) . '");');
+  } else {
+    $value_field = new Input('configuration_value', ['value' => $cInfo->configuration_value]);
+  }
+
+  $contents = ['form' => new Form('configuration', (clone $GLOBALS['link'])->set_parameter('action', 'save'))];
+  $contents[] = ['text' => TEXT_INFO_EDIT_INTRO];
+  $contents[] = ['text' => '<strong>' . $cInfo->configuration_title . '</strong><br>' . $cInfo->configuration_description . '<br>' . $value_field];
+  $contents[] = [
+    'class' => 'text-center',
+    'text' => new Button(IMAGE_SAVE, 'fas fa-save', 'btn-success mr-2')
+            . $GLOBALS['Admin']->button(IMAGE_CANCEL, 'fas fa-times', 'btn-light', $GLOBALS['link']),
+  ];
