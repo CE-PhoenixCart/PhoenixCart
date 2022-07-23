0816c7df1df1d13207236b8458d5a850bb954ce9
Matt Fletcher <104613+ecartz@users.noreply.github.com>
2022-07-23 00:31:01 +0000

    Modernize

--- includes/hooks/admin/languages/add_pages.php
+++ includes/hooks/admin/languages/add_pages.php
@@ -13,15 +13,15 @@
 class hook_admin_languages_add_pages {
 
   public function listen_insertAction() {
-    global $lID, $languages_id;
+    global $lID;
 
-    tep_db_query("INSERT INTO pages_description (pages_id, languages_id) SELECT pages_id, " . (int)$lID . " FROM pages_description WHERE languages_id = " . (int)$languages_id);
+    $GLOBALS['db']->query("INSERT INTO pages_description (pages_id, languages_id) SELECT pages_id, " . (int)$lID . " FROM pages_description WHERE languages_id = " . (int)$_SESSION['languages_id']);
   }
 
   public function listen_deleteConfirmAction() {
     global $lID;
 
-    tep_db_query("DELETE FROM pages_description WHERE languages_id = '" . (int)$lID . "'");
+    $GLOBALS['db']->query("DELETE FROM pages_description WHERE languages_id = " . (int)$lID);
   }
 
 }
