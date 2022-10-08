ff79343675c9822260e93c029729a76d8a5a844c
Matt Fletcher <104613+ecartz@users.noreply.github.com>
2022-10-08 14:02:53 +0000

    Fix language deletion
    Fixes a bug where the default language check logic was backward (so
    that the error would show when the language was not the default).
    
    Converts deletion to a POST form rather than a GET link.  Not only
    is this good practice in general (write operations should be POST
    rather than GET), it makes the action processing work without further
    manipulation.

--- admin/includes/actions/languages/infoboxes/delete.php
+++ admin/includes/actions/languages/infoboxes/delete.php
@@ -14,6 +14,12 @@
   $heading = TEXT_INFO_HEADING_DELETE_LANGUAGE;
   $link = $GLOBALS['link']->set_parameter('lID', (int)$lInfo->languages_id);
 
+  if ($GLOBALS['remove_language']) {
+    $contents = ['form' => new Form('languages', (clone $link)->set_parameter('action', 'delete_confirm'))];
+    $button = new Button(IMAGE_DELETE, 'fas fa-trash', 'btn-danger mr-2');
+  } else {
+    $button = '';
+  }
   $contents[] = ['text' => TEXT_INFO_DELETE_INTRO];
   $contents[] = [
     'class' => 'text-center text-uppercase font-weight-bold',
@@ -21,6 +27,6 @@
   ];
   $contents[] = [
     'class' => 'text-center',
-    'text' => (($remove_language) ? $GLOBALS['Admin']->button(IMAGE_DELETE, 'fas fa-trash', 'btn-danger mr-2', (clone $link)->set_parameter('action', 'delete_confirm')) : '')
+    'text' => $button
             . $GLOBALS['Admin']->button(IMAGE_CANCEL, 'fas fa-times', 'btn-light', $link),
   ];
