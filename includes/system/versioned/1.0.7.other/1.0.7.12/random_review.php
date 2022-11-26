f4eb87ab0eeadc137ec9c202dbc5ded6480c1c19
Matt Fletcher <104613+ecartz@users.noreply.github.com>
2022-11-26 15:02:56 +0000

    Only show enabled reviews in sidebox
    Previously, we returned an offset of zero when there were no reviews
    that were enabled (either because there were no reviews or because
    all reviews were disabled).  This changes to return no results, so we
    don't even try to load a review.
    
    There was also a bug where disabled reviews weren't excluded from the
    second query.  This oculd cause a disabled review to be shown rather
    than the enabled review chosen by the first query.  This also
    interacted with the offset of zero issue to show the first disabled
    review when there were no enabled reviews.
    
    With these changes, the review sidebox will never show a disabled
    review.  And it won't show at all unless there is at least one
    enabled review.

--- includes/system/versioned/1.0.7.other/1.0.7.12/random_review.php
+++ includes/system/versioned/1.0.7.other/1.0.7.12/random_review.php
@@ -20,6 +20,7 @@ SELECT RAND() * COUNT(*) AS `offset`
    INNER JOIN products p ON p.products_id = r.products_id
    INNER JOIN products_description pd ON p.products_id = pd.products_id AND rd.languages_id = pd.language_id
   WHERE p.products_status = 1 AND r.reviews_status = 1 AND rd.languages_id = %d
+  HAVING COUNT(*) > 0
   ORDER BY r.reviews_id DESC
 EOSQL
         , (int)$_SESSION['languages_id']));
@@ -39,7 +40,7 @@ SELECT %s,
     INNER JOIN products_description pd ON p.products_id = pd.products_id AND rd.languages_id = pd.language_id
     LEFT JOIN specials s ON p.products_id = s.products_id
     LEFT JOIN (SELECT products_id, COUNT(*) AS attribute_count FROM products_attributes GROUP BY products_id) a ON p.products_id = a.products_id
-  WHERE p.products_status = 1 AND pd.language_id = %d
+  WHERE p.products_status = 1 AND r.reviews_status = 1 AND rd.languages_id = %d
   ORDER BY r.reviews_id DESC LIMIT 1 OFFSET %d
 EOSQL
         , Product::COLUMNS, (int)$_SESSION['languages_id'], (int)$random_selection['offset']));
