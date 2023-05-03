47c2cc11b4f2d3af69fdef0287f589c1b4e8f9b0
gburton <gb8231059@gmail.com>
2023-05-03 11:07:56 +0100

    Remove HTML from Class File
    To allow for easier templating

---
+++ includes/system/versioned/1.0.8.other/1.0.8.21/order_total.php
@@ -0,0 +1,71 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  class order_total {
+
+    public $modules;
+
+// class constructor
+    function __construct() {
+      if (defined('MODULE_ORDER_TOTAL_INSTALLED') && !Text::is_empty(MODULE_ORDER_TOTAL_INSTALLED)) {
+        $this->modules = explode(';', MODULE_ORDER_TOTAL_INSTALLED);
+
+        foreach ($this->modules as $value) {
+          $class = pathinfo($value, PATHINFO_FILENAME);
+          $GLOBALS[$class] = new $class();
+        }
+      }
+    }
+
+    function process() {
+      $order_total_array = [];
+      if (is_array($this->modules)) {
+        foreach ($this->modules as $value) {
+          $class = pathinfo($value, PATHINFO_FILENAME);
+          if ($GLOBALS[$class]->enabled) {
+            $GLOBALS[$class]->output = [];
+            $GLOBALS[$class]->process();
+
+            foreach ($GLOBALS[$class]->output as $output) {
+              if (!Text::is_empty($output['title']) && !Text::is_empty($output['text'])) {
+                $order_total_array[] = [
+                  'code' => $GLOBALS[$class]->code,
+                  'title' => $output['title'],
+                  'text' => $output['text'],
+                  'value' => $output['value'],
+                  'sort_order' => $GLOBALS[$class]->sort_order,
+                ];
+              }
+            }
+          }
+        }
+      }
+
+      return $order_total_array;
+    }
+
+    function output() {
+      $output_string = '';
+      if (is_array($this->modules)) {
+        foreach ($this->modules as $value) {
+          $class = pathinfo($value, PATHINFO_FILENAME);
+          if ($GLOBALS[$class]->enabled) {
+            foreach ($GLOBALS[$class]->output as $output) {
+              include Guarantor::ensure_global('Template')->map('order_total.php', 'component');
+            }
+          }
+        }
+      }
+
+      return $output_string;
+    }
+  }
