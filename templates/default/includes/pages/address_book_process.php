fe95a6e58050b0661c511dc56b840affdd0e8226
ecartz <104613+ecartz@users.noreply.github.com>
2023-06-18 07:34:57 -0400

    Fix first Back button's missing parameters

--- templates/default/includes/pages/address_book_process.php
+++ templates/default/includes/pages/address_book_process.php
@@ -44,7 +44,7 @@
 
   <div class="buttonSet">
     <div class="text-right"><?= new Button(IMAGE_BUTTON_DELETE, 'fas fa-trash-alt', 'btn-danger btn-lg btn-block', [], $Linker->build('address_book_process.php', ['delete' => $_GET['delete'], 'action' => 'deleteconfirm', 'formid' => $_SESSION['sessiontoken']])) ?></div>
-    <p><?= new Button(IMAGE_BUTTON_BACK, 'fas fa-angle-left', $Linker->build('address_book.php')) ?></p>
+    <p><?= new Button(IMAGE_BUTTON_BACK, 'fas fa-angle-left', '', [], $Linker->build('address_book.php')) ?></p>
   </div>
 
 <?php
