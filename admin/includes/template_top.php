c4f27661c01a9d964592fceaf952f61ac36c00ba
gburton <gb8231059@gmail.com>
2023-11-10 10:04:12 +0000

    Load jQueryUI only where needed
    This loads jQueryUI only on catalog.php.  We do not use jQueryUI CSS, so no need to load at all.

--- admin/includes/template_top.php
+++ admin/includes/template_top.php
@@ -18,11 +18,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title><?= TITLE ?></title>
 <base href="<?= HTTP_SERVER . DIR_WS_ADMIN ?>" />
-<link rel="stylesheet" href="<?= $Admin->catalog('ext/jquery/ui/redmond/jquery-ui-1.10.4.min.css') ?>">
 
 <?= $admin_hooks->cat('injectSiteStart') ?>
-
-<script src="<?= $Admin->catalog('ext/jquery/ui/jquery-ui-1.10.4.min.js') ?>"></script>
 
 </head>
 <body>
---
+++ includes/hooks/admin/catalog/jQueryUI.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+class hook_admin_catalog_jQueryUI {
+
+  public $version = '1.13.2';
+
+  public function listen_injectSiteStart() {
+    $jQueryUI = '<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js" integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>';
+    
+    return $jQueryUI;
+  }
+
+}
