fbfe6c0f4a6ca3e48b96180fb1f02c56c0953585
gburton <gb8231059@gmail.com>
2023-11-16 11:07:37 +0000

    Star Rating Text
    Make more accessible for screen-readers and the like.

--- includes/languages/english.php
+++ includes/languages/english.php
@@ -105,3 +105,5 @@ const PAYMENT_FA_ICON = '<i class="fas fa-file-invoice-dollar fa-fw fa-3x float-
 
 const ENTRY_COMMENTS = 'Anything we need to know?';
 const ENTRY_COMMENTS_PLACEHOLDER = 'Comment here...';
+
+const STAR_RATING = 'Rated %s Stars';
