e965ff9cd937a2db8ee65c186ee6d88ea827133a
gburton <gb8231059@gmail.com>
2023-11-22 13:29:54 +0000

    Update sce_admin_http_authentication.php
    Fix extended check script to return same results as dashboard module.

--- admin/includes/modules/security_check/extended/sce_admin_http_authentication.php
+++ admin/includes/modules/security_check/extended/sce_admin_http_authentication.php
@@ -19,7 +19,32 @@
     }
 
     public function pass() {
-      return isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+      $is_iis = stripos($_SERVER['SERVER_SOFTWARE'], 'iis');
+      
+      $htaccess_path = DIR_FS_ADMIN . '.htaccess';
+      $htpasswd_path = DIR_FS_ADMIN . '.htpasswd_phoenix';
+      
+      $htaccess_lines = [];
+      if (!$is_iis && file_exists($htpasswd_path) && Path::is_writable($htpasswd_path) && file_exists($htaccess_path) && Path::is_writable($htaccess_path)) {
+        if (filesize($htaccess_path) > 0) {
+          $htaccess_lines = explode("\n", file_get_contents($htaccess_path));
+        }
+
+        $htpasswd_lines = (filesize($htpasswd_path) > 0) ? explode("\n", file_get_contents($htpasswd_path)) : [];
+      } else {
+        $htpasswd_lines = false;
+      }
+      
+      if (is_array($htpasswd_lines)) {
+        if (empty($htpasswd_lines)) {
+          return false;
+        }
+        
+        return true;
+      }
+      else if (!$is_iis) {
+        return false;
+      }      
     }
 
     public function get_message() {
