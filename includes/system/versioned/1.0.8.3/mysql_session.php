6415a255fa8e7fda6c05d5c67a846443fe7518e7
ecartz <104613+ecartz@users.noreply.github.com>
2023-12-24 23:28:06 -0500

    Finish session implementation
    Fix return values for gc and read to match the return type of the
    interface.  Add return types for the last two methods.

--- includes/system/versioned/1.0.8.3/mysql_session.php
+++ includes/system/versioned/1.0.8.3/mysql_session.php
@@ -22,7 +22,8 @@
 
     #[\ReturnTypeWillChange]
     public function gc($maxlifetime) {
-      return false !== $GLOBALS['db']->query("DELETE FROM sessions WHERE expiry < '" . (int)(time() - $maxlifetime) . "'");
+      $query = $GLOBALS['db']->query("DELETE FROM sessions WHERE expiry < '" . (int)(time() - $maxlifetime) . "'");
+      return $query ? mysqli_num_rows($query) : false;
     }
 
     public function open($save_path, $session_name) : bool {
@@ -34,21 +35,19 @@
       $value_query = $GLOBALS['db']->query("SELECT value FROM sessions WHERE sesskey = '" . $GLOBALS['db']->escape($key) . "'");
       $value = $value_query->fetch_assoc();
 
-      return $value['value'] ?? '';
+      return $value['value'] ?? false;
     }
 
     public function updateTimestamp($key, $ignore) : bool {
       return false !== $GLOBALS['db']->query("UPDATE sessions SET expiry = " . (int)time() . " WHERE sesskey = '" . $GLOBALS['db']->escape($key) . "'");
     }
 
-    #[\ReturnTypeWillChange]
-    public function validateId($key) {
+    public function validateId($key) : bool {
       $query = $GLOBALS['db']->query("SELECT expiry FROM sessions WHERE sesskey = '" . $GLOBALS['db']->escape($key) . "'");
       return mysqli_num_rows($query) > 0;
     }
 
-    #[\ReturnTypeWillChange]
-    public function write($key, $value) {
+    public function write($key, $value) : bool {
       return false !== $GLOBALS['db']->query("INSERT INTO sessions (sesskey, expiry, value) VALUES ('"
         . $GLOBALS['db']->escape($key) . "', " . (int)time() . ", '" . $GLOBALS['db']->escape($value)
         . "') ON DUPLICATE KEY UPDATE expiry = VALUES(expiry), value = VALUES(value)");
