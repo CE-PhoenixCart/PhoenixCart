eeec4e907cea3d5baecd685049b39aaaddff946f
gburton <gb8231059@gmail.com>
2024-01-04 12:04:09 +0000

    Date language not needed
    The new style datepicker should not have (eg) YYYY-MM-DD helper text as datepickers will show in users own format determined by HTML5 / browser.
    
    Dates were removed from advanced_search a long time ago, so these constants can be removed.

--- includes/languages/english/advanced_search.php
+++ includes/languages/english/advanced_search.php
@@ -24,13 +24,10 @@ const ENTRY_INCLUDE_SUBCATEGORIES = 'Include Subcategories';
 const ENTRY_MANUFACTURERS = 'Manufacturers';
 const ENTRY_PRICE_FROM = 'Price From';
 const ENTRY_PRICE_TO = 'Price To';
-const ENTRY_DATE_FROM = 'Date From';
-const ENTRY_DATE_TO = 'Date To';
 
 const ENTRY_PRICE_FROM_TEXT = 'From';
 const ENTRY_PRICE_TO_TEXT = 'To';
 
-const ENTRY_DATE = 'Date';
 const ENTRY_PRICE = 'Price';
 
 const TEXT_SEARCH_HELP_LINK = '<i class="fas fa-info-circle"></i> Search Help';
@@ -57,12 +54,7 @@ const TEXT_CLOSE_WINDOW = '<u>Close Window</u> [x]';
 const TEXT_NO_PRODUCTS = 'There is no product that matches the search criteria.';
 
 const ERROR_AT_LEAST_ONE_INPUT = 'At least one of the fields in the search form must be entered.';
-const ERROR_INVALID_FROM_DATE = 'Invalid From Date.';
-const ERROR_INVALID_TO_DATE = 'Invalid To Date.';
-const ERROR_TO_DATE_LESS_THAN_FROM_DATE = 'To Date must be greater than or equal to From Date.';
 const ERROR_PRICE_FROM_MUST_BE_NUM = 'Price From must be a number.';
 const ERROR_PRICE_TO_MUST_BE_NUM = 'Price To must be a number.';
 const ERROR_PRICE_TO_LESS_THAN_PRICE_FROM = 'Price To must be greater than or equal to Price From.';
 const ERROR_INVALID_KEYWORDS = 'Invalid keywords.';
-
-const DATE_FORMAT_STRING = 'mm/dd/yyyy';
