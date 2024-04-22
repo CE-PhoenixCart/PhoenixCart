1c235e790bab00efeb65879ae22edb24147192db
gburton <gb8231059@gmail.com>
2024-04-22 10:59:55 +0100

    Update stats_products_viewed.php
    Update page to modern Phoenix codebase

--- admin/stats_products_viewed.php
+++ admin/stats_products_viewed.php
@@ -13,6 +13,47 @@
   require 'includes/application_top.php';
 
   require 'includes/template_top.php';
+  
+  $products_sql = "SELECT p.products_id, pd.products_name, pd.products_viewed, l.name FROM products p, products_description pd, languages l WHERE p.products_id = pd.products_id AND l.languages_id = pd.language_id ORDER BY pd.products_viewed DESC";
+
+  $link = $Admin->link('catalog.php', ['action' => 'new_product_preview', 'read' => 'only', 'origin' => 'stats_products_viewed.php?page=' . (int)($_GET['page'] ?? 1)]);
+   
+  $table_definition = [
+    'columns' => [
+      [
+        'name' => TABLE_HEADING_NUMBER,
+        'class' => 'col-1',
+        'function' => function ($row) {
+          static $count = 0;
+          return str_pad(++$count, 2, '0', STR_PAD_LEFT) . '.';
+        },
+      ],
+      [
+        'name' => TABLE_HEADING_PRODUCTS,
+        'function' => function ($row) {
+          return '<a target="_blank" class="stretched-link" href="' . $row['onclick'] . '">' . $row['products_name'] . '</a>';
+        },
+      ],
+      [
+        'name' => TABLE_HEADING_VIEWED,
+        'class' => 'text-right',
+        'function' => function ($row) {
+          return $row['products_viewed'];
+        },
+      ],
+    ],
+    'count_text' => TEXT_DISPLAY_NUMBER_OF_PRODUCTS,
+    'page' => $_GET['page'] ?? null,
+    'rows_per_page' => MAX_DISPLAY_SEARCH_RESULTS,
+    'sql' => $products_sql,
+    'width' => 12,
+  ];
+  
+  $table_definition['split'] = new Paginator($table_definition);
+  $table_definition['function'] = function (&$row) use ($link, &$table_definition) {
+    $row['onclick'] = $link->set_parameter('pID', (int)$row['products_id']);
+    $row['css'] = '';
+  };  
 ?>
 
   <div class="row">
@@ -26,43 +67,9 @@
     </div>
   </div>
 
-  <div class="table-responsive">
-    <table class="table table-striped table-hover">
-      <thead class="thead-dark">
-        <tr>
-          <th><?= TABLE_HEADING_NUMBER ?></th>
-          <th><?= TABLE_HEADING_PRODUCTS ?></th>
-          <th class="text-right"><?= TABLE_HEADING_VIEWED ?></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $rows = 0;
-        $products_sql = "SELECT p.products_id, pd.products_name, pd.products_viewed, l.name FROM products p, products_description pd, languages l WHERE p.products_id = pd.products_id AND l.languages_id = pd.language_id ORDER BY pd.products_viewed DESC";
-        $products_split = new splitPageResults($_GET['page'], MAX_DISPLAY_SEARCH_RESULTS, $products_sql, $products_query_numrows);
-        $link = $Admin->link('catalog.php', ['action' => 'new_product_preview', 'read' => 'only', 'origin' => 'stats_products_viewed.php?page=' . (int)$_GET['page']]);
-        $products_query = $db->query($products_sql);
-        while ($products = $products_query->fetch_assoc()) {
-          $rows++;
-          ?>
-          <tr onclick="document.location.href='<?= $link->set_parameter('pID', (int)$products['products_id']) ?>'">
-            <td><?= str_pad($rows, 2, '0', STR_PAD_LEFT) ?>.</td>
-            <td><?= '<a href="' . $link . '">' . $products['products_name'] . '</a> (' . $products['name'] . ')' ?></td>
-            <td class="text-right"><?= $products['products_viewed'] ?></td>
-          </tr>
-          <?php
-        }
-        ?>
-      </tbody>
-    </table>
-  </div>
-
-  <div class="row">
-    <div class="col-sm-6"><?= $products_split->display_count($products_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, $_GET['page'], TEXT_DISPLAY_NUMBER_OF_PRODUCTS) ?></div>
-    <div class="col-sm-6 text-sm-right"><?= $products_split->display_links($products_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, MAX_DISPLAY_PAGE_LINKS, $_GET['page']) ?></div>
-  </div>
-
 <?php
+  $table_definition['split']->display_table();
+  
   require 'includes/template_bottom.php';
   require 'includes/application_bottom.php';
 ?>
