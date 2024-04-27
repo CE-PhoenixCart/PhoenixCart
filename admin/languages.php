d53345801f8cbcad7a66910a0e103dc642856ab6
gburton <gb8231059@gmail.com>
2024-04-27 19:16:32 +0100

    Languages to View

--- admin/languages.php
+++ admin/languages.php
@@ -29,68 +29,6 @@
 
   require 'includes/segments/process_action.php';
 
-  $table_definition = [
-    'columns' => [
-      [
-        'name' => TABLE_HEADING_LANGUAGE_NAME,
-        'is_heading' => true,
-        'function' => function ($row) {
-          return $row['name']
-               . ((DEFAULT_LANGUAGE == $row['code'])
-                ? ' (' . TEXT_DEFAULT . ')'
-                : '');
-        },
-      ],
-      [
-        'name' => TABLE_HEADING_LANGUAGE_CODE,
-        'function' => function ($row) {
-          return $row['code'];
-        },
-      ],
-      [
-        'name' => TABLE_HEADING_LANGUAGE_IMAGE,
-        'function' => function ($row) use ($Admin) {
-          return $Admin->catalog_image("includes/languages/{$row['directory']}/images/{$row['image']}");
-        },
-      ],
-      [
-        'name' => TABLE_HEADING_ACTION,
-        'class' => 'text-right',
-        'function' => function ($row) {
-          return (isset($row['info']->languages_id) && ($row['languages_id'] == $row['info']->languages_id) )
-               ? '<i class="fas fa-chevron-circle-right text-info"></i>'
-               : '<a href="' . $row['onclick'] . '"><i class="fas fa-info-circle text-muted"></i></a>';
-        },
-      ],
-    ],
-    'count_text' => TEXT_DISPLAY_NUMBER_OF_LANGUAGES,
-    'page' => $_GET['page'] ?? null,
-    'web_id' => 'lID',
-    'db_id' => 'languages_id',
-    'rows_per_page' => MAX_DISPLAY_SEARCH_RESULTS,
-    'sql' => "SELECT * FROM languages ORDER BY sort_order",
-  ];
-
-  $table_definition['function'] = function (&$row) use (&$table_definition) {
-    $GLOBALS['link']->set_parameter('lID', (int)$row['languages_id']);
-
-    if (!isset($table_definition['info'])
-      && (!isset($_GET['lID']) || ($_GET['lID'] == $row['languages_id']))
-      && !Text::is_prefixed_by($GLOBALS['action'], 'new'))
-    {
-      $table_definition['info'] = new objectInfo($row);
-      $row['info'] = &$table_definition['info'];
-
-      $row['css'] = ' class="table-active"';
-      $row['onclick'] = (clone $GLOBALS['link'])->set_parameter('action', 'edit');
-    } else {
-      $row['css'] = '';
-      $row['onclick'] = $GLOBALS['link'];
-    }
-  };
-
-  $table_definition['split'] = new Paginator($table_definition);
-
   require 'includes/template_top.php';
 ?>
 
@@ -111,7 +49,9 @@
   </div>
 
 <?php
-  $table_definition['split']->display_table();
+  if ($view_file = $Admin->locate('/views', $action)) {
+    require $view_file;
+  }
 
   require 'includes/template_bottom.php';
   require 'includes/application_bottom.php';
