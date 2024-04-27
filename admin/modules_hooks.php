af23aa7494e8598f1cc916748fa1fb70743acfbb
gburton <gb8231059@gmail.com>
2024-04-27 20:15:48 +0100

    Hooks to View

--- admin/modules_hooks.php
+++ admin/modules_hooks.php
@@ -114,61 +114,11 @@ EOSQL
     </div>
   </div>
 
-  <div class="table-responsive">
-    <table class="table table-striped table-hover">
-      <?php
-  foreach ( $contents as $site => $groups ) {
-?>
-      <thead class="thead-dark">
-        <tr>
-          <th colspan="4"><?php printf(TABLE_HEADING_LOCATION, $site); ?></th>
-        </tr>
-      </thead>
-      <thead class="thead-light">
-        <tr>
-          <th><?= TABLE_HEADING_GROUP ?></th>
-          <th><?= TABLE_HEADING_FILE ?></th>
-          <th><?= TABLE_HEADING_METHOD ?></th>
-          <th class="text-right"><?= TABLE_HEADING_VERSION ?></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-    foreach ( $groups as $group => $actions ) {
-      foreach ( $actions as $action => $codes ) {
-        foreach ( $codes as $code => $locations) {
-          foreach ($locations as $location) {
-            if (is_array($location)) {
-              $file = implode('->', $location);
-              $class = explode('::', $location[0])[0];
-              $version = class_exists($class) ? (get_class_vars($class)['version'] ?? null) : null;
-            } else {
-              $file = "$code.php";
-              $version = get_class_vars("hook_{$site}_{$group}_{$code}")['version'] ?? null;
-            }
-?>
-        <tr>
-          <td><?= $group ?></td>
-          <td><?= $file ?></td>
-          <td><?= $action ?></td>
-          <td class="text-right"><?= $version ?? 'N/A' ?></td>
-        </tr>
-        <?php
-          }
-        }
-      }
-    }
-  }
-?>
-      </tbody>
-    </table>
-  </div>
-
-  <hr>
-
-  <p><?= TEXT_HOOKS_DIRECTORY . ' ' . DIR_FS_CATALOG . 'includes/hooks/' ?></p>
-
 <?php
+  if ($view_file = $Admin->locate('/views', $action)) {
+    require $view_file;
+  }
+  
   require 'includes/template_bottom.php';
   require 'includes/application_bottom.php';
 ?>
