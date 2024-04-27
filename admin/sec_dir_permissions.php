c7639dcb80abc29f57c92a83dd80b56866301bf1
gburton <gb8231059@gmail.com>
2024-04-27 09:34:12 +0100

    Security Directory Permissions to View

--- admin/sec_dir_permissions.php
+++ admin/sec_dir_permissions.php
@@ -71,39 +71,11 @@
     </div>
   </div>
 
-  <div class="table-responsive">
-    <table class="table table-striped table-hover">
-      <thead class="thead-dark">
-        <tr>
-          <th><?= TABLE_HEADING_DIRECTORIES ?></th>
-          <th class="text-center"><?= TABLE_HEADING_RECOMMENDED ?></th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php
-      foreach (generate_phoenix_directories(DIR_FS_CATALOG) as $file) {
-        if (empty($_GET['show_all']) && ($file['whitelisted'] == $file['writable'])) {
-          continue;
-        }
-      ?>
-        <tr>
-          <td><?= Text::ltrim_once($file['name'], DIR_FS_CATALOG) ?></td>
-          <td class="text-center"><i class="fas fa-<?=
-            $file['whitelisted'] ? 'edit' : 'lock',
-            ' text-',
-            ($file['whitelisted'] == $file['writable']) ? 'success' : 'danger'
-          ?>"></i></td>
-        </tr>
-        <?php
-      }
-      ?>
-      </tbody>
-    </table>
-  </div>
-
-  <p><?= sprintf(TEXT_DIRECTORY, DIR_FS_CATALOG) ?></p>
-
 <?php
+  if ($view_file = $Admin->locate('/views', $action)) {
+    require $view_file;
+  }
+  
   require 'includes/template_bottom.php';
   require 'includes/application_bottom.php';
 ?>
