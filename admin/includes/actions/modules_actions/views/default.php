bb6d323e3e410282483feead6a953c8103e109d8
gburton <gb8231059@gmail.com>
2024-04-27 20:18:20 +0100

    Actions to Views

---
+++ admin/includes/actions/modules_actions/views/default.php
@@ -0,0 +1,47 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+?>
+
+    <div class="table-responsive">
+    <table class="table table-striped table-hover">
+      <thead class="thead-dark">
+        <tr>
+          <th><?= TABLE_HEADING_FILE ?></th>
+          <th><?= TABLE_HEADING_ACTION ?></th>
+          <th><?= TABLE_HEADING_CLASS ?></th>
+          <th><?= TABLE_HEADING_METHOD ?></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        foreach (array_diff(scandir($directory), ['.', '..']) as $file) {
+          $action = pathinfo($file, PATHINFO_FILENAME);
+          $class = "\\Phoenix\\Actions\\$action";
+
+          foreach (get_class_methods($class) as $method) {
+            ?>
+            <tr>
+              <td><?= $file ?></td>
+              <td><?= $action ?></td>
+              <td><?= $class ?></td>
+              <td><?= $method ?></td>
+            </tr>
+          <?php
+          }
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+
+  <p><?= TEXT_ACTIONS_DIRECTORY . ' ' . DIR_FS_CATALOG . 'includes/actions/' ?></p>
+  
--- admin/modules_actions.php
+++ admin/modules_actions.php
@@ -29,41 +29,11 @@
     </div>
   </div>
 
-  <div class="table-responsive">
-    <table class="table table-striped table-hover">
-      <thead class="thead-dark">
-        <tr>
-          <th><?= TABLE_HEADING_FILE ?></th>
-          <th><?= TABLE_HEADING_ACTION ?></th>
-          <th><?= TABLE_HEADING_CLASS ?></th>
-          <th><?= TABLE_HEADING_METHOD ?></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        foreach (array_diff(scandir($directory), ['.', '..']) as $file) {
-          $action = pathinfo($file, PATHINFO_FILENAME);
-          $class = "\\Phoenix\\Actions\\$action";
-
-          foreach (get_class_methods($class) as $method) {
-            ?>
-            <tr>
-              <td><?= $file ?></td>
-              <td><?= $action ?></td>
-              <td><?= $class ?></td>
-              <td><?= $method ?></td>
-            </tr>
-          <?php
-          }
-        }
-        ?>
-      </tbody>
-    </table>
-  </div>
-
-  <p><?= TEXT_ACTIONS_DIRECTORY . ' ' . DIR_FS_CATALOG . 'includes/actions/' ?></p>
-
 <?php
+  if ($view_file = $Admin->locate('/views', $action)) {
+    require $view_file;
+  }
+  
   require 'includes/template_bottom.php';
   require 'includes/application_bottom.php';
 ?>
