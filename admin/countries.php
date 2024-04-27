e8d403f5a49270dcc13618821d1679f1c10888ed
gburton <gb8231059@gmail.com>
2024-04-27 09:21:09 +0100

    Countries to View

--- admin/countries.php
+++ admin/countries.php
@@ -11,67 +11,12 @@
 */
 
   $always_valid_actions = ['set_flag'];
+  
   require 'includes/application_top.php';
+  
   $link = $Admin->link()->retain_query_except(['action', 'cID']);
+  
   require 'includes/segments/process_action.php';
-
-  $table_definition = [
-    'columns' => [
-      [
-        'name' => TABLE_HEADING_COUNTRY_NAME,
-        'is_heading' => true,
-        'function' => function ($row) {
-          return $row['countries_name'];
-        },
-      ],
-      [
-        'name' => TABLE_HEADING_COUNTRY_CODES,
-        'function' => function ($row) {
-          return implode(', ', [$row['countries_iso_code_2'], $row['countries_iso_code_3']]);
-        },
-      ],
-      [
-        'name' => TABLE_HEADING_STATUS,
-        'class' => 'text-right',
-        'function' => function (&$row) {
-          $href = (clone $row['onclick'])->set_parameter('action', 'set_flag');
-          return ($row['status'] == '1')
-               ? '<i class="fas fa-check-circle text-success"></i> <a href="' . $href->set_parameter('flag', '0')  . '"><i class="fas fa-times-circle text-muted"></i></a>'
-               : '<a href="' . $href->set_parameter('flag', '1') . '"><i class="fas fa-check-circle text-muted"></i></a> <i class="fas fa-times-circle text-danger"></i>';
-        },
-      ],
-      [
-        'name' => TABLE_HEADING_ACTION,
-        'class' => 'text-right',
-        'function' => function ($row) {
-          return ((isset($row['info']->countries_id))
-                ? '<i class="fas fa-chevron-circle-right text-info"></i>'
-                : '<a href="' . $row['onclick'] . '"><i class="fas fa-info-circle text-muted"></i></a>');
-        },
-      ],
-    ],
-    'count_text' => TEXT_DISPLAY_NUMBER_OF_COUNTRIES,
-    'page' => $_GET['page'] ?? null,
-    'web_id' => 'cID',
-    'sql' => "SELECT * FROM countries ORDER BY countries_name",
-  ];
-
-  $table_definition['split'] = new Paginator($table_definition);
-  $link = $Admin->link()->retain_query_except(['action']);
-  $table_definition['function'] = function (&$row) use ($link, $action, &$table_definition) {
-    $link->set_parameter('cID', $row['countries_id']);
-    if (!isset($table_definition['info']) && (!isset($_GET['cID']) || ($_GET['cID'] == $row['countries_id'])) && (substr($action, 0, 3) !== 'new')) {
-      $table_definition['info'] = new objectInfo($row);
-      $row['info'] = &$table_definition['info'];
-
-      $row['onclick'] = (clone $link)->set_parameter('action', 'edit');
-      $row['css'] = ' class="table-active"';
-      $row['info']->link = $link;
-    } else {
-      $row['onclick'] = $link;
-      $row['css'] = '';
-    }
-  };
 
   require 'includes/template_top.php';
 ?>
@@ -92,7 +37,9 @@
   </div>
 
   <?php
-  $table_definition['split']->display_table();
+  if ($view_file = $Admin->locate('/views', $action)) {
+    require $view_file;
+  }
 
   require 'includes/template_bottom.php';
   require 'includes/application_bottom.php';
