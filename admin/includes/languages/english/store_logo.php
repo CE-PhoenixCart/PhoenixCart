6627fe2cddc2dfade34343a14052e8eecfc5bf4b
gburton <gb8231059@gmail.com>
2024-04-28 13:31:41 +0100

    Store Logo to View
    This also adds extra descriptive text.

--- admin/includes/languages/english/store_logo.php
+++ admin/includes/languages/english/store_logo.php
@@ -12,12 +12,14 @@
 
 const HEADING_TITLE = 'Store Logo';
 
-const HEADING_NEW_LOGO = 'Upload a new Logo';
+const TEXT_EXISTING_LOGO = '<b>This is your existing Store Logo.</b><br><br>It is used both in your Shop & in your Admin area (eg on your invoice and packing slip.';
+
+const BUTTON_EDIT_LOGO = 'Upload a New Logo';
 
 const TEXT_LOGO_IMAGE = 'New Logo';
-const TEXT_FORMAT_AND_LOCATION = 'The store logo must be in one of png/gif/jpg/svg/webp formats and will OVERWRITE the existing Logo';
+const TEXT_FORMAT_AND_LOCATION = '<b>Your existing Logo will be OVERWRITTEN.</b><br><br>The new Store Logo must be in one of png/gif/jpg/svg/webp formats.';
 
-const SUCCESS_LOGO_UPDATED = '<strong>Success:</strong> The store logo has successfully been updated!';
+const SUCCESS_LOGO_UPDATED = '<strong>Success:</strong> The Store Logo has successfully been updated!';
 
 const ERROR_IMAGES_DIRECTORY_NOT_WRITEABLE = '<strong>Error:</strong> The images directory cannot be uploaded to. (<a href="%s">click here to review the directory permissions</a>)';
 
--- admin/store_logo.php
+++ admin/store_logo.php
@@ -27,54 +27,20 @@
     </div>
     <div class="col-12 col-lg-4 text-left text-lg-right align-self-center pb-1">
       <?=
-      $Admin->button(GET_HELP, '', 'btn-dark', GET_HELP_LINK, ['newwindow' => true]),
-      $admin_hooks->cat('extraButtons')
+      $Admin->button(GET_HELP, '', 'btn-dark mr-2', GET_HELP_LINK, ['newwindow' => true]),
+      $admin_hooks->cat('extraButtons'),
+      empty($action)
+      ? $Admin->button(BUTTON_EDIT_LOGO, 'fas fa-id-card', 'btn-danger', $Admin->link('store_logo.php', ['action' => 'edit']))
+      : $Admin->button(IMAGE_BACK, 'fas fa-angle-left', 'btn-light', $Admin->link('store_logo.php'))
       ?>
     </div>
   </div>
 
-  <?= $Admin->catalog_image('images/' .  STORE_LOGO) ?>
-  <small><?= DIR_FS_CATALOG . 'images/' .  STORE_LOGO ?></small>
-  <hr>
-  
-  <h3 class="display-4 mb-2"><?= HEADING_NEW_LOGO ?></h3>
-    
-  <div class="row">
-    <div class="col">
-      <?= new Form('logo', $Admin->link()->set_parameter('action', 'save'), 'post', ['enctype' => 'multipart/form-data']) ?>
-
-        <div class="custom-file mb-2">
-          <?= (new Input('store_logo', ['accept' => 'image/*', 'id' => 'inputLogo', 'class' => 'custom-file-input'], 'file'))->require() ?>
-
-          <label class="custom-file-label" for="inputLogo"><?= TEXT_LOGO_IMAGE ?></label>
-        </div>
-
-        <?= $admin_hooks->cat('editForm'),
-            new Button(IMAGE_UPLOAD, 'fas fa-file-upload', 'btn-danger btn-block')
-        ?>
-
-      </form>
-    </div>
-    <div class="col">
-      <div class="alert alert-danger"><?= TEXT_FORMAT_AND_LOCATION ?></div>
-    </div>
-  </div>
-
-  <script>
-  var upload = document.querySelector('#inputLogo');
-  if (upload) {
-    upload.addEventListener('change', function (event) {
-      var labels = document.querySelectorAll('LABEL.custom-file-label');
-      for (var i = 0; i < labels.length; i++) {
-        if ('inputLogo' === labels[i].htmlFor) {
-          labels[i].innerHTML = event.target.files[0].name;
-        }
-      }
-    });
-  }
-  </script>
-
 <?php
+  if ($view_file = $Admin->locate('/views', $action)) {
+    require $view_file;
+  }
+  
   require 'includes/template_bottom.php';
   require 'includes/application_bottom.php';
 ?>
