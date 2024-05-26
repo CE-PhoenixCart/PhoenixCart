18dd9b921879b8790e41b4df2e5a8cc2bc3b47e5
ecartz <104613+ecartz@users.noreply.github.com>
2024-05-26 08:37:53 -0400

    Match class name to file name

--- includes/system/versioned/1.0.9.3/url_query.php
+++ includes/system/versioned/1.0.9.3/url_query.php
@@ -10,7 +10,7 @@
   Released under the GNU General Public License
 */
 
-  class html_query {
+  class url_query {
 
     public static function parse(string $query) : array {
       parse_str($query, $parameters);
