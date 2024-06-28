66b608992c99ab3c11a3c96801fedbd97d2e81ca
gburton <gb8231059@gmail.com>
2024-06-28 11:45:35 +0100

    Introduce Carousel

---
+++ includes/languages/english/modules/content/index/cm_i_slider.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_I_SLIDER_TITLE         = 'Carousel';
+  const MODULE_CONTENT_I_SLIDER_DESCRIPTION   = 'Shows a series of slides as a Carousel.  Links into the core advert_manager.';
+
+  const MODULE_CONTENT_I_SLIDER_CONTROLS_PREV = 'Previous';
+  const MODULE_CONTENT_I_SLIDER_CONTROLS_NEXT = 'Next';
---
+++ includes/modules/content/index/templates/tpl_cm_i_slider.php
@@ -0,0 +1,22 @@
+<div class="<?= MODULE_CONTENT_I_SLIDER_CONTENT_WIDTH ?> cm-i-slider">
+  <div id="cmislider" class="carousel slide<?= $cm_i_slider_fade ?>" data-ride="carousel">
+    <?= $cm_i_indicator_output ?>
+    <div class="carousel-inner">
+      <?= $cm_i_slider_output ?>
+    </div>
+    <?= $cm_i_slider_controls ?>
+  </div>
+</div>
+
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+?>
