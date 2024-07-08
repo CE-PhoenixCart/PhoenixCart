426979a97538dcbac917bd5453fcbe6cb072ab4c
gburton <gb8231059@gmail.com>
2024-07-08 11:53:49 +0100

    Tap Target Shopping Cart
    Also aligns items per row

--- includes/modules/content/shopping_cart/templates/tpl_cm_sc_product_listing.php
+++ includes/modules/content/shopping_cart/templates/tpl_cm_sc_product_listing.php
@@ -9,6 +9,7 @@
           <th><?= MODULE_CONTENT_SC_PRODUCT_LISTING_HEADING_AVAILABILITY ?></th>
           <th><?= MODULE_CONTENT_SC_PRODUCT_LISTING_HEADING_QUANTITY ?></th>
           <th class="text-right"><?= MODULE_CONTENT_SC_PRODUCT_LISTING_HEADING_PRICE ?></th>
+          <th>&nbsp;</th>
         </tr>
       </thead>
       <tbody>
@@ -18,7 +19,7 @@
     echo '<td class="d-none d-md-table-cell"><a href="', $product->get('link'), '">',
          new Image('images/' . $product->get('image'), [], htmlspecialchars($product->get('name')), SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT),
          '</a></td>';
-    echo '<th><a href="', $product->get('link'), '">', $product->get('name'), '</a>';
+    echo '<th class="align-middle"><a href="', $product->get('link'), '">', $product->get('name'), '</a>';
     $attributes = $product->get('attributes');
     foreach (($product->get('attribute_selections') ?? []) as $option => $value) {
       echo '<small><br><i> - ', $attributes[$option]['name'], ' ', $attributes[$option]['values'][$value]['name'], '</i></small>';
@@ -28,22 +29,20 @@
     if (STOCK_CHECK == 'true' && $product->lacks_stock()) {
       $GLOBALS['any_out_of_stock'] = true;
 
-      echo '<td><span class="text-danger"><b>' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . '</b></span></td>';
+      echo '<td class="align-middle"><span class="text-danger"><b>' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . '</b></span></td>';
     } else {
-      echo '<td>' . MODULE_CONTENT_SC_PRODUCT_LISTING_TEXT_IN_STOCK . '</td>';
+      echo '<td class="align-middle">' . MODULE_CONTENT_SC_PRODUCT_LISTING_TEXT_IN_STOCK . '</td>';
     }
 
-    echo '<td>';
+    echo '<td class="align-middle">';
     echo '<div class="input-group">';
     echo new Input('cart_quantity[]', ['value' => $product->get('quantity'), 'style' => 'width: 65px;', 'min' => '0'], 'number');
     echo new Input('products_id[]', ['value' => $product->get('uprid')], 'hidden');
     echo '<div class="input-group-append">', new Button(MODULE_CONTENT_SC_PRODUCT_LISTING_TEXT_BUTTON_UPDATE, '', 'btn-info'), '</div>';
-    echo '<div class="input-group-append">',
-         new Button(MODULE_CONTENT_SC_PRODUCT_LISTING_TEXT_BUTTON_REMOVE, '', 'btn-danger', [], $GLOBALS['Linker']->build('shopping_cart.php', ['action' => 'remove_product', 'products_id' => $product->get('uprid')])),
-         '</div>';
     echo '</div>';
     echo '</td>';
-    echo '<td class="text-right">', $product->format('final_price', $product->get('quantity')), '</td>';
+    echo '<td class="text-right align-middle">', $product->format('final_price', $product->get('quantity')), '</td>';
+    echo '<td class="text-right align-middle">', new Button(MODULE_CONTENT_SC_PRODUCT_LISTING_TEXT_BUTTON_REMOVE, '', 'btn-link', [], $GLOBALS['Linker']->build('shopping_cart.php', ['action' => 'remove_product', 'products_id' => $product->get('uprid')])), '</td>';
     echo '</tr>';
   }
 ?>
