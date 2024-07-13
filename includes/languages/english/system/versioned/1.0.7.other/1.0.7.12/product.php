83ff184be0d02d96c644c58424b87ef4f2556dd1
gburton <gb8231059@gmail.com>
2024-07-13 13:00:37 +0100

    Update product.php
    Remove word View, for a better layout.

--- includes/languages/english/system/versioned/1.0.7.other/1.0.7.12/product.php
+++ includes/languages/english/system/versioned/1.0.7.other/1.0.7.12/product.php
@@ -12,5 +12,5 @@
 
 const IS_PRODUCT_SHOW_PRICE = '%s';
 const IS_PRODUCT_SHOW_PRICE_SPECIAL = '<del>%s</del> <span class="text-danger">now %s</span>';
-const IS_PRODUCT_BUTTON_BUY = '<i class="fas fa-shopping-cart"></i>';
-const IS_PRODUCT_BUTTON_VIEW = '<i class="fas fa-eye"></i> View';
+const IS_PRODUCT_BUTTON_BUY = '<i class="fas fa-shopping-cart" title="Add To Cart"></i>';
+const IS_PRODUCT_BUTTON_VIEW = '<i class="fas fa-eye" title="View Product"></i>';
