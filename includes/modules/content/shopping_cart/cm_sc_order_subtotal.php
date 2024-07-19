ee1defb3da747ef71369fc5d585eae0b3695e956
gburton <gb8231059@gmail.com>
2024-07-19 09:22:30 +0100

    Reduce margin-top

--- includes/modules/content/shopping_cart/cm_sc_order_subtotal.php
+++ includes/modules/content/shopping_cart/cm_sc_order_subtotal.php
@@ -48,7 +48,7 @@
         ],
         $this->config_key_base . 'CONTENT_WIDTH' => [
           'title' => 'Content Container',
-          'value' => 'col-sm-12 mt-5',
+          'value' => 'col-sm-12 mt-2',
           'desc' => 'What container should the content be shown in? (col-*-12 = full width, col-*-6 = half width).',
         ],
         $this->config_key_base . 'SORT_ORDER' => [
