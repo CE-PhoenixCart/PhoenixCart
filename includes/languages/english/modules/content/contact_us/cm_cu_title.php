4f46997cdb5b2f918f0cf895620ee8b46bdacd78
gburton <gb8231059@gmail.com>
2024-07-19 12:31:26 +0100

    Contact Us Content Modules

---
+++ includes/languages/english/modules/content/contact_us/cm_cu_title.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_CU_TITLE_TITLE = 'Page Heading';
+  const MODULE_CONTENT_CU_TITLE_DESCRIPTION = 'Shows the Title of the Page';
+  
+  const MODULE_CONTENT_CU_TITLE_PUBLIC_TITLE = 'Contact Us';
---
+++ includes/modules/content/contact_us/templates/tpl_cm_cu_title.php
@@ -0,0 +1,16 @@
+<div class="<?= MODULE_CONTENT_CU_TITLE_CONTENT_WIDTH ?> cm-cu-title">
+  <h1 class="display-4"><?= MODULE_CONTENT_CU_TITLE_PUBLIC_TITLE ?></h1>
+</div>
+
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+?>
