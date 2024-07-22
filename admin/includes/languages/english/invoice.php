00cee75736cbd13e0881e2c5cd9d5d469a59fcd8
gburton <gb8231059@gmail.com>
2024-07-22 09:48:09 +0100

    Match Language Billing/Delivery

--- admin/includes/languages/english/invoice.php
+++ admin/includes/languages/english/invoice.php
@@ -22,8 +22,8 @@ const TABLE_HEADING_PRICE_INCLUDING_TAX = 'Price (inc)';
 const TABLE_HEADING_TOTAL_EXCLUDING_TAX = 'Total (ex)';
 const TABLE_HEADING_TOTAL_INCLUDING_TAX = 'Total (inc)';
 
-const ENTRY_SOLD_TO = 'SOLD TO:';
-const ENTRY_SHIP_TO = 'SHIP TO:';
+const ENTRY_SOLD_TO = 'Billing Address:';
+const ENTRY_SHIP_TO = 'Delivery Address:';
 const ENTRY_PAYMENT_METHOD = 'Payment Method: %s';
 const ENTRY_SUB_TOTAL = 'Sub-Total:';
 const ENTRY_TAX = 'Tax:';
--- admin/includes/languages/english/packingslip.php
+++ admin/includes/languages/english/packingslip.php
@@ -15,8 +15,8 @@ const TABLE_HEADING_PRODUCTS_MODEL = 'Model';
 const TABLE_HEADING_PRODUCTS = 'Products';
 const TABLE_HEADING_QTY = 'Qty';
 
-const ENTRY_SOLD_TO = 'SOLD TO:';
-const ENTRY_SHIP_TO = 'SHIP TO:';
+const ENTRY_SOLD_TO = 'Billing Address:';
+const ENTRY_SHIP_TO = 'Delivery Address:';
 const ENTRY_PAYMENT_METHOD = 'Payment Method: %s';
 
 const ENTRY_INVOICE_NUMBER = 'Order ID: %s';
