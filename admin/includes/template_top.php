c04a2a5be985a00e74cddaa103a3e1473698e8c0
gburton <gb8231059@gmail.com>
2024-07-24 11:13:10 +0100

    Add Page Name to title
    If user has multiple admin pages open in browser, this change will allow her to more easily locate pages.

--- admin/includes/template_top.php
+++ admin/includes/template_top.php
@@ -16,7 +16,7 @@
 <meta charset="<?= CHARSET ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title><?= TITLE ?></title>
+<title><?= (defined('HEADING_TITLE') ? strip_tags(HEADING_TITLE) . ' | ' : '') . TITLE ?></title>
 <base href="<?= HTTP_SERVER . DIR_WS_ADMIN ?>" />
 <link rel="icon" type="image/png" href="images/icon_phoenix.png">
 <?= $admin_hooks->cat('injectSiteStart') ?>
