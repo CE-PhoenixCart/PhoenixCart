cb61c5cf6374f844ce6de1d95fecdff96f5c3daf
gburton <gb8231059@gmail.com>
2024-08-07 10:19:34 +0100

    Update tpl_bm_manufacturer_info.php
    Use $link for all links.  Clean up format.

--- includes/modules/boxes/templates/tpl_bm_manufacturer_info.php
+++ includes/modules/boxes/templates/tpl_bm_manufacturer_info.php
@@ -2,31 +2,31 @@
   <div class="card-header">
     <?= MODULE_BOXES_MANUFACTURER_INFO_BOX_TITLE ?>
   </div>
-<?php
-  if (!Text::is_empty($_image)) {
-?>
-  <a href="<?= $GLOBALS['Linker']->build('index.php', ['manufacturers_id' => (int)$_id]) ?>"><?=
-    new Image("images/$_image", ['class' => 'card-img-top'], htmlspecialchars($_brand), SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT)
-  ?></a>
-<?php
-  }
-
+  <?php
   $link = $GLOBALS['Linker']->build('index.php', ['manufacturers_id' => (int)$_id]);
-?>
+  
+  if (!Text::is_empty($_image)) {
+    ?>
+    <a href="<?= $link ?>"><?= new Image("images/$_image", ['class' => 'card-img-top'], htmlspecialchars($_brand), SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) ?></a>
+    <?php
+  }
+  ?>
+  
   <div class="card-body">
-    <h5 class="card-title mb-0"><a href="<?= $GLOBALS['Linker']->build('index.php', ['manufacturers_id' => (int)$_id]) ?>"><?= $_brand ?></a></h5>
+    <h5 class="card-title mb-0"><a href="<?= $link ?>"><?= $_brand ?></a></h5>
   </div>
   <div class="list-group list-group-flush">
     <a class="list-group-item list-group-item-action text-muted" href="<?= $link ?>"><?= MODULE_BOXES_MANUFACTURER_INFO_BOX_OTHER_PRODUCTS ?></a>
-<?php
-  if (!Text::is_empty($_url)) {
-    $link = $GLOBALS['Linker']->build('redirect.php', ['action' => 'manufacturer', 'manufacturers_id' => (int)$_id]);
-?>
 
-    <a class="list-group-item list-group-item-action text-muted" href="<?= $link ?>" target="_blank"><?= sprintf(MODULE_BOXES_MANUFACTURER_INFO_BOX_HOMEPAGE, $_brand) ?></a>
-<?php
-  }
-?>
+    <?php
+    if (!Text::is_empty($_url)) {
+      $link = $GLOBALS['Linker']->build('redirect.php', ['action' => 'manufacturer', 'manufacturers_id' => (int)$_id]);
+      ?>
+
+      <a class="list-group-item list-group-item-action text-muted" href="<?= $link ?>" target="_blank"><?= sprintf(MODULE_BOXES_MANUFACTURER_INFO_BOX_HOMEPAGE, $_brand) ?></a>
+      <?php
+    }
+    ?>
   </div>
 </div>
 
