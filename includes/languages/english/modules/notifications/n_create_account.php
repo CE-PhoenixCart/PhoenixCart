92947c1d0d2ee10bfdf9317f3146fd1ccb64f30a
gburton <gb8231059@gmail.com>
2024-08-17 09:28:32 +0100

    Update n_create_account.php
    Accidental removal of the WELCOME paragraph.

--- includes/languages/english/modules/notifications/n_create_account.php
+++ includes/languages/english/modules/notifications/n_create_account.php
@@ -14,7 +14,7 @@ const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT_TITLE = 'Create Profile';
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT_DESCRIPTION = 'Send a notification at the completion of profile creation.';
 
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_SUBJECT = 'Welcome to ' . STORE_NAME;
-const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WELCOME = '';
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WELCOME = 'Welcome to <strong>' . STORE_NAME . '</strong>.' . "\n\n";
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT = 'You\'ve successfully registered your details with us.' . "\n\n" . 'Next time you shop with us, just sign in for a faster checkout.' . "\n\n";
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_CONTACT = 'For help with any of our online services, please e-mail the store-owner: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n";
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WARNING = '<strong>Note:</strong> This e-mail address was given to us by one of our customers. If you did not sign up to be a member, please send an e-mail to ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n";
