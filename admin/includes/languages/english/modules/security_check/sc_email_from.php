d6d1446365f7d2123023994535f79652f6f21e91
gburton <gb8231059@gmail.com>
2024-09-06 12:15:10 +0100

    Email From Check
    If Email From is not explicitly set, emails may be undelivered.  This module will check if EMAIL_FROM is set to default, and if so, create a check message.

---
+++ admin/includes/languages/english/modules/security_check/sc_email_from.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+const MODULE_SECURITY_CHECK_EMAIL_FROM_TITLE = 'email_from';
+const MODULE_SECURITY_CHECK_EMAIL_FROM_ERROR = <<<'EOT'
+You must set "E-mail From"<br>
+Admin > E-mail Options > E-mail From<br>
+This e-mail address should be the same domain as your shop, and have a working inbox (set up by you or your host).  
+Eg if your shop is www.xyzshop.com, your email address could be info@xyzshop.com
+EOT;
