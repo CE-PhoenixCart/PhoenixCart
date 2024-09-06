5bd0eadb7c4216875232cffbe8d92d7f35eff0bf
gburton <gb8231059@gmail.com>
2024-09-06 23:05:56 +0100

    Add Version
    Add version to footer

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -14,6 +14,6 @@
 
 <footer class="col mt-3 d-print-none">
   <div class="card bg-light p-0 pt-3 mb-3 card-body text-center">
-    <p>CE Phoenix &copy; 2000-<?= date('Y') ?> <a href="https://www.phoenixcart.org" target="_blank" rel="noreferrer">Phoenix Cart</a></p>
+    <p>CE Phoenix v<?= Versions::get('Phoenix') ?> &copy; 2000-<?= date('Y') ?> <a href="https://www.phoenixcart.org" target="_blank" rel="noreferrer">Phoenix Cart</a></p>
   </div>
 </footer>
