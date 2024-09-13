dab0d85c4de54fc433dc79ee62d6dff170995d4a
gburton <gb8231059@gmail.com>
2024-09-13 12:38:55 +0100

    Update FA to 6.6.0

--- templates/default/includes/hooks/shop/siteWide/fontAwesome.php
+++ templates/default/includes/hooks/shop/siteWide/fontAwesome.php
@@ -11,12 +11,12 @@
 */
 
 class hook_shop_siteWide_fontAwesome {
-  public $version = '6.4.2';
+  public $version = '6.6.0';
 
   public $sitestart = null;
 
   public function listen_injectSiteStart() {
-    $this->sitestart .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />' . PHP_EOL;
+    $this->sitestart .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />' . PHP_EOL;
 
     return $this->sitestart;
   }
