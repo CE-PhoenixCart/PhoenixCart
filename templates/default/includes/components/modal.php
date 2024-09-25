8d0b904a3fd5bc4e30b734896fd385242ecf45ce
gburton <gb8231059@gmail.com>
2024-09-25 13:03:29 +0100

    Update modal.php
    Modal XL

--- templates/default/includes/components/modal.php
+++ templates/default/includes/components/modal.php
@@ -12,7 +12,7 @@
 ?>
 
 <div class="modal fade" id="<?= $modal['name'] ?>" tabindex="-1" role="dialog" aria-labelledby="<?= $modal['name'] ?>Label" aria-hidden="true">
-  <div class="modal-dialog" role="document"><div class="modal-content">
+  <div class="modal-dialog modal-xl" role="document"><div class="modal-content">
     <div class="modal-header">
       <h5 class="modal-title" id="<?= $modal['name'] ?>Label"><?= $modal['title'] ?></h5>
       <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
