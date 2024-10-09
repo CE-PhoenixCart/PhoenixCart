385e3757b98ed368d44ccb6679313da35eee4321
gburton <gb8231059@gmail.com>
2024-10-09 17:23:22 +0100

    Minor Display Cosmetics
    Add margin bottom to Layout Modules.
    Solve Display Row on product_info

--- includes/modules/pi/product_info/pi_options_attributes.php
+++ includes/modules/pi/product_info/pi_options_attributes.php
@@ -88,7 +88,7 @@
         ],
         $this->config_key_base . 'CONTENT_WIDTH' => [
           'title' => 'Content Container',
-          'value' => 'col-sm-12 mt-2',
+          'value' => 'col-sm-12 mb-2',
           'desc' => 'What container should the content be shown in? (col-*-12 = full width, col-*-6 = half width).',
         ],
         $this->config_key_base . 'HELPER' => [
