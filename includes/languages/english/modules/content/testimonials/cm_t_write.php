05d7b7186f552c39d74c29c4593755bf54b732c3
gburton <gb8231059@gmail.com>
2024-11-11 11:59:20 +0000

    Introduce Write Testimonial
    Installed by default on the testimonials page.

---
+++ includes/languages/english/modules/content/testimonials/cm_t_write.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_TITLE        = 'Write Testimonial';
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_DESCRIPTION  = 'Allow customer to write a testimonial.';
+
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_BUTTON_TEXT = '<i class="fas fa-pen mr-2 me-2"></i>Write a Testimonial';
+
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_PUBLIC_TEXT = 'Tell us what you\'d like us to know.  These comments may become public on this page, so you might like to use a Nickname rather than your real name.';
+
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_NICKNAME = 'Your name';
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_NICKNAME_PLACEHOLDER = 'Your name (or nickname)';
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_TEXT = 'Testimonial';
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_TEXT_PLACEHOLDER = 'What would you like to say?';
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_BUTTON_TEXT_SEND = 'Send to shopowner';
+
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_THANK_YOU = 'Thank you %s, your Testimonial has been sent to the Shopowner.';
+  
