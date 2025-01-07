af87e85c7b5b08d63777ba738f3ac7f414555390
gburton <gb8231059@gmail.com>
2025-01-07 16:51:50 +0000

    Fix Gallery Modal

--- templates/default/includes/hooks/shop/product_info/gallery.php
+++ templates/default/includes/hooks/shop/product_info/gallery.php
@@ -13,66 +13,68 @@
 class hook_shop_product_info_gallery {
   
   public function listen_injectBodyEnd() {
-    if (defined('MODULE_CONTENT_PI_GALLERY_STATUS') && ('True' === MODULE_CONTENT_PI_GALLERY_STATUS)) {
-      $swipe_arrows = MODULE_CONTENT_PI_GALLERY_SWIPE_ARROWS;
-      $gallery_indicators = MODULE_CONTENT_PI_GALLERY_INDICATORS;
-      
-      $modal_size = MODULE_CONTENT_PI_GALLERY_MODAL_SIZE;
-      $album_name = sprintf(MODULE_CONTENT_PI_GALLERY_ALBUM_NAME, $GLOBALS['product']->get('name'));
-      $album_exit = MODULE_CONTENT_PI_GALLERY_ALBUM_CLOSE;
-    }
-    elseif (defined('PI_GALLERY_STATUS') && ('True' === PI_GALLERY_STATUS)) {
-      $swipe_arrows = GALLERY_SWIPE_ARROWS;
-      $gallery_indicators = GALLERY_INDICATORS;
-      
-      $modal_size = GALLERY_MODAL_SIZE;
-      $album_name = sprintf(GALLERY_ALBUM_NAME, $GLOBALS['product']->get('name'));
-      $album_exit = GALLERY_ALBUM_CLOSE;
-    }
-    else {
-      $swipe_arrows = GALLERY_SWIPE_ARROWS;
-      $gallery_indicators = GALLERY_INDICATORS;
-      
-      $modal_size = GALLERY_MODAL_SIZE;
-      $album_name = sprintf(GALLERY_ALBUM_NAME, $GLOBALS['product']->get('name'));
-      $album_exit = GALLERY_ALBUM_CLOSE;
-    }
-
-    $first_img = new Image('images/' . $GLOBALS['product']->get('image'), ['alt' => htmlspecialchars($GLOBALS['product']->get('name')), 'loading' => 'lazy']);
-      
-    $other_images = $GLOBALS['db']->fetch_all("SELECT image, htmlcontent FROM products_images WHERE products_id = '" . (int)$GLOBALS['product']->get('id') . "' ORDER BY sort_order");
-    
-    $other_img_indicator = $other_img = '';
-    
-    if (count($other_images) > 0) {
-      foreach ($other_images as $k => $v) {
-        $other_img .= '<div class="carousel-item text-center">';
-        $other_img .= new Image('images/' . $v['image'], ['alt' => htmlspecialchars($GLOBALS['product']->get('name')), 'loading' => 'lazy']);
-        if (!Text::is_empty($v['htmlcontent'])) {
-          $other_img .= '<div class="carousel-caption d-none d-md-block">';
-            $other_img .= $v['htmlcontent'];
-          $other_img .= '</div>';
+    if (isset($GLOBALS['product']) && ($GLOBALS['product'] instanceof Product)) {
+      if ($GLOBALS['product']->get('status') == 1) {
+        if (defined('MODULE_CONTENT_PI_GALLERY_STATUS') && ('True' === MODULE_CONTENT_PI_GALLERY_STATUS)) {
+          $swipe_arrows = MODULE_CONTENT_PI_GALLERY_SWIPE_ARROWS;
+          $gallery_indicators = MODULE_CONTENT_PI_GALLERY_INDICATORS;
+          
+          $modal_size = MODULE_CONTENT_PI_GALLERY_MODAL_SIZE;
+          $album_name = sprintf(MODULE_CONTENT_PI_GALLERY_ALBUM_NAME, $GLOBALS['product']->get('name'));
+          $album_exit = MODULE_CONTENT_PI_GALLERY_ALBUM_CLOSE;
         }
-        $other_img .= '</div>';
-      }
-    }
+        elseif (defined('PI_GALLERY_STATUS') && ('True' === PI_GALLERY_STATUS)) {
+          $swipe_arrows = GALLERY_SWIPE_ARROWS;
+          $gallery_indicators = GALLERY_INDICATORS;
+          
+          $modal_size = GALLERY_MODAL_SIZE;
+          $album_name = sprintf(GALLERY_ALBUM_NAME, $GLOBALS['product']->get('name'));
+          $album_exit = GALLERY_ALBUM_CLOSE;
+        }
+        else {
+          $swipe_arrows = GALLERY_SWIPE_ARROWS;
+          $gallery_indicators = GALLERY_INDICATORS;
+          
+          $modal_size = GALLERY_MODAL_SIZE;
+          $album_name = sprintf(GALLERY_ALBUM_NAME, $GLOBALS['product']->get('name'));
+          $album_exit = GALLERY_ALBUM_CLOSE;
+        }
+
+        $first_img = new Image('images/' . $GLOBALS['product']->get('image'), ['alt' => htmlspecialchars($GLOBALS['product']->get('name')), 'loading' => 'lazy']);
+          
+        $other_images = $GLOBALS['db']->fetch_all("SELECT image, htmlcontent FROM products_images WHERE products_id = '" . (int)$GLOBALS['product']->get('id') . "' ORDER BY sort_order");
         
-    $display_swipe_arrows = $display_indicators = '';
-    
-    if ($swipe_arrows === 'True') {
-      $display_swipe_arrows = '<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev"><span class="border border-white bg-secondary rounded" aria-hidden="true"><span class="carousel-control-prev-icon mt-1"></span></span></a><a class="carousel-control-next" href="#carousel" role="button" data-slide="next"><span class="border border-white bg-secondary rounded" aria-hidden="true"><span class="carousel-control-next-icon mt-1"></span></span></a>';
-    } 
-
-    if ($gallery_indicators === 'True') {
-      $display_indicators = '<ol class="carousel-indicators">';
-        $display_indicators .= '<li data-target="#carousel" data-slide-to="0" class="pointer border border-white bg-secondary rounded active"></li>';
-        for ($i = 1, $n = count($other_images); $i <= $n; $i++) {
-          $display_indicators .= '<li data-target="#carousel" data-slide-to="' . $i . '" class="pointer border border-white bg-secondary rounded"></li>';
+        $other_img_indicator = $other_img = '';
+        
+        if (count($other_images) > 0) {
+          foreach ($other_images as $k => $v) {
+            $other_img .= '<div class="carousel-item text-center">';
+            $other_img .= new Image('images/' . $v['image'], ['alt' => htmlspecialchars($GLOBALS['product']->get('name')), 'loading' => 'lazy']);
+            if (!Text::is_empty($v['htmlcontent'])) {
+              $other_img .= '<div class="carousel-caption d-none d-md-block">';
+                $other_img .= $v['htmlcontent'];
+              $other_img .= '</div>';
+            }
+            $other_img .= '</div>';
+          }
         }
-      $display_indicators .= '</ol>';
-    }
+        
+        $display_swipe_arrows = $display_indicators = '';
+        
+        if ($swipe_arrows === 'True') {
+          $display_swipe_arrows = '<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev"><span class="border border-white bg-secondary rounded" aria-hidden="true"><span class="carousel-control-prev-icon mt-1"></span></span></a><a class="carousel-control-next" href="#carousel" role="button" data-slide="next"><span class="border border-white bg-secondary rounded" aria-hidden="true"><span class="carousel-control-next-icon mt-1"></span></span></a>';
+        } 
 
-    $modal_gallery_footer = <<<mgf
+        if ($gallery_indicators === 'True') {
+          $display_indicators = '<ol class="carousel-indicators">';
+            $display_indicators .= '<li data-target="#carousel" data-slide-to="0" class="pointer border border-white bg-secondary rounded active"></li>';
+            for ($i = 1, $n = count($other_images); $i <= $n; $i++) {
+              $display_indicators .= '<li data-target="#carousel" data-slide-to="' . $i . '" class="pointer border border-white bg-secondary rounded"></li>';
+            }
+          $display_indicators .= '</ol>';
+        }
+
+        $modal_gallery_footer = <<<mgf
 <div id="lightbox" class="modal fade" role="dialog">
   <div class="modal-dialog {$modal_size}" role="document">
     <div class="modal-content">
@@ -95,7 +97,9 @@ class hook_shop_product_info_gallery {
 </div>
 mgf;
 
-    return $modal_gallery_footer;
+        return $modal_gallery_footer;
+      }
+    }
   }
 
 }
