35f02532058d449f3467f312cb3f9132c7f64c4a
gburton <gb8231059@gmail.com>
2025-03-13 22:50:57 +0000

    Carry over missing CSS selectors
    Missing span.productPrice.
    Added span.specialPrice.

--- includes/languages/english/system/versioned/1.0.7.other/1.0.7.12/product.php
+++ includes/languages/english/system/versioned/1.0.7.other/1.0.7.12/product.php
@@ -10,7 +10,7 @@
   Released under the GNU General Public License
 */
 
-const IS_PRODUCT_SHOW_PRICE = '%s';
-const IS_PRODUCT_SHOW_PRICE_SPECIAL = '<span class="sr-only visually-hidden">original price</span><s>%s</s> <span class="sr-only visually-hidden">sale price</span><span class="text-danger">%s</span>';
+const IS_PRODUCT_SHOW_PRICE = '<span class="productPrice">%s</span>';
+const IS_PRODUCT_SHOW_PRICE_SPECIAL = '<span class="sr-only visually-hidden">original price</span><s><span class="specialPrice">%s</span></s> <span class="sr-only visually-hidden">sale price</span><span class="text-danger productPrice">%s</span>';
 const IS_PRODUCT_BUTTON_BUY = '<i class="fas fa-shopping-cart" title="Add To Cart"></i>';
 const IS_PRODUCT_BUTTON_VIEW = '<i class="fas fa-eye" title="View Product"></i>';
