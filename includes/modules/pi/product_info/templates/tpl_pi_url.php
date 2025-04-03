eba592b559d5714871ff1484a69a80306149ea41
gburton <gb8231059@gmail.com>
2025-04-03 12:27:00 +0100

    Introduce Product URL layout module

---
+++ includes/modules/pi/product_info/templates/tpl_pi_url.php
@@ -0,0 +1,20 @@
+<div class="<?= PI_URL_CONTENT_WIDTH ?> pi-url">
+  <ul class="list-group">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <?= sprintf(PI_URL_DISPLAY_URL, $products_url) ?>
+    </li>
+  </ul>
+</div>
+
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+?>
