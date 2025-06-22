1b4ef5e8aadf75ece7d13787b14541fde83f2aa9
gburton <gb8231059@gmail.com>
2025-06-22 23:02:08 +0100

    Bump Bootstrap to 5.3.7

--- templates/default/includes/hooks/shop/siteWide/bootStrap.php
+++ templates/default/includes/hooks/shop/siteWide/bootStrap.php
@@ -12,16 +12,16 @@
 
 class hook_shop_siteWide_bootStrap {
 
-  public $version = '5.3.6';
+  public $version = '5.3.7';
 
   function listen_injectSiteStart() {
-    return '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">' . PHP_EOL;
+    return '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">' . PHP_EOL;
   }
 
   function listen_injectSiteEnd() {
     return <<<'bs'
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js" integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js" integrity="sha384-7qAoOXltbVP82dhxHAUje59V5r2YsVfBafyUDxEdApLPmcdhBPg1DKg1ERo0BZlK" crossorigin="anonymous"></script>
 bs;
   }
 
