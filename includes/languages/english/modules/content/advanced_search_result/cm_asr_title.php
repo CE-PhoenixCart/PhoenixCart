c706e778e2f55544ac9792f380586bd6b17bd5db
gburton <gb8231059@gmail.com>
2026-01-28 12:41:44 +0000

    Search Title

---
+++ includes/languages/english/modules/content/advanced_search_result/cm_asr_title.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2026 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_ASR_TITLE_TITLE = 'Title';
+  const MODULE_CONTENT_ASR_TITLE_DESCRIPTION = 'Shows the Title of the Page';
+  
+  const MODULE_CONTENT_ASR_TITLE_PUBLIC_TITLE = 'Results for <strong>%s</strong>';
