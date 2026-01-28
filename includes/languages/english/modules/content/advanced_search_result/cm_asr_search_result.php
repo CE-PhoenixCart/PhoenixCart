9df1485752a1c38bd73bee8b47269b7c5e0d5a64
gburton <gb8231059@gmail.com>
2026-01-28 11:47:12 +0000

    Search Results

---
+++ includes/languages/english/modules/content/advanced_search_result/cm_asr_search_result.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2026 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_ASR_SEARCH_RESULT_TITLE = 'Search Results';
+  const MODULE_CONTENT_ASR_SEARCH_RESULT_DESCRIPTION = 'Shows the Search Keywords Product Listing.';
+
