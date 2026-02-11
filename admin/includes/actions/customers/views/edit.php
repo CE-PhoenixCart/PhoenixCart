58fb9ae9730f1710ea5a41e57be1c2ef0cdc2a66
gburton <gb8231059@gmail.com>
2026-02-11 16:14:12 +0000

    Update Customers to Tabbed Functionality

--- admin/includes/actions/customers/views/edit.php
+++ admin/includes/actions/customers/views/edit.php
@@ -14,16 +14,26 @@
   
   $hooks =& $admin_hooks;
   $Template = new Template('default');
+  ?>
+  
+  <div id="customerTabs">
+    <ul class="nav nav-tabs">
+      <li class="nav-item"><a class="nav-link active" data-bs-toggle="tab" href="#section_customer_details" role="tab"><?= TAB_TITLE_CUSTOMER_DETAILS ?></a></li>
+    </ul>
     
-  echo (new Form('customers', $Admin->link('customers.php')->retain_query_except()->set_parameter('action', 'update'), 'post'))
+    <div class="tab-content pt-3">
+      <div class="tab-pane fade show active" id="section_customer_details" role="tabpanel">
+        <?php
+        echo (new Form('customers', $Admin->link('customers.php')->retain_query_except()->set_parameter('action', 'update'), 'post'))
       ->hide('default_address_id', $customer_data->get('default_address_id', $customer_details));
 
-  $cwd = getcwd();
-  chdir(DIR_FS_CATALOG);
+        echo '<div class="row">';
+          $cwd = getcwd();
+          chdir(DIR_FS_CATALOG);
 
-  $page_fields = $customer_data->get_fields_for_page('customers');
-  $grouped_modules = $customer_data->get_grouped_modules();
-  $customer_data_group_query = $db->query(sprintf(<<<'EOSQL'
+          $page_fields = $customer_data->get_fields_for_page('customers');
+          $grouped_modules = $customer_data->get_grouped_modules();
+          $customer_data_group_query = $db->query(sprintf(<<<'EOSQL'
 SELECT *
  FROM customer_data_groups
  WHERE language_id = %d
@@ -31,30 +41,39 @@ SELECT *
 EOSQL
       , (int)$_SESSION['languages_id']));
 
-  while ($customer_data_group = $customer_data_group_query->fetch_assoc()) {
-    if (empty($grouped_modules[$customer_data_group['customer_data_groups_id']])) {
-      continue;
-    }
-    ?>
+            while ($customer_data_group = $customer_data_group_query->fetch_assoc()) {
+              if (empty($grouped_modules[$customer_data_group['customer_data_groups_id']])) {
+                continue;
+              }
+              ?>
+              <div class="<?= $customer_data_group['customer_data_groups_width'] ?>">
+                <p class="fs-4 fw-semibold mb-1"><?= $customer_data_group['customer_data_groups_name'] ?></p>
 
-   <h5><?= $customer_data_group['customer_data_groups_name'] ?></h5>
+                <?php
+                foreach ((array)$grouped_modules[$customer_data_group['customer_data_groups_id']] as $module) {
+                  if (count(array_intersect(get_class($module)::PROVIDES, $page_fields)) > 0) {
+                    $module->display_input($customer_details);
+                  }
+                }
+                ?>
+              </div>
+              <?php
+            }
 
-    <?php
-    foreach ((array)$grouped_modules[$customer_data_group['customer_data_groups_id']] as $module) {
-      if (count(array_intersect(get_class($module)::PROVIDES, $page_fields)) > 0) {
-        $module->display_input($customer_details);
-      }
-    }
-  }
+            chdir($cwd);
 
-  chdir($cwd);
-
-  echo $admin_hooks->cat('editForm');
-  echo $admin_hooks->cat('injectFormDisplay');
-  ?>
-  
-  <div class="d-grid mt-2">
-    <?= new Button(IMAGE_SAVE, 'fas fa-save', 'btn-success btn-lg') ?>
+            echo $admin_hooks->cat('editForm');
+            echo $admin_hooks->cat('injectFormDisplay');
+            ?>
+          
+            <div class="d-grid mt-2">
+              <?= new Button(IMAGE_SAVE, 'fas fa-save', 'btn-success') ?>
+            </div>
+          </div>
+        </form>
+      </div>
+      
+      <?= $admin_hooks->cat('customerTab') ?>
+      
+    </div>
   </div>
-  
-  </form>
