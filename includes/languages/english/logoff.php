6e9413000c5a4f5064235852755e5ee29def6d5f
gburton <gb8231059@gmail.com>
2026-03-13 12:54:00 +0000

    Update logoff.php
    Modernise Text

--- includes/languages/english/logoff.php
+++ includes/languages/english/logoff.php
@@ -13,6 +13,6 @@
 const HEADING_TITLE = 'Sign Out';
 const NAVBAR_TITLE = 'Sign Out';
 const TEXT_MAIN = <<<'EOT'
-You have successfully signed out of your profile.  It is now safe to leave the computer.<br><br>
-Your shopping cart has been saved, the items inside it will be restored whenever you sign in to your profile.
+You're now signed out.
+Your cart has been saved - sign in anytime to complete your order.
 EOT;
