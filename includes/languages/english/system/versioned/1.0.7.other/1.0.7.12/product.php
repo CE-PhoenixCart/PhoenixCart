1654e629b699cccf4a763e019c5ad8b462500dcd
gburton <gb8231059@gmail.com>
2026-03-24 12:35:22 +0000

    Choose Options Button
    If a product has attributes, this adds a "choose options" button.

--- includes/languages/english/system/versioned/1.0.7.other/1.0.7.12/product.php
+++ includes/languages/english/system/versioned/1.0.7.other/1.0.7.12/product.php
@@ -14,3 +14,5 @@ const IS_PRODUCT_SHOW_PRICE = '<span class="productPrice">%s</span>';
 const IS_PRODUCT_SHOW_PRICE_SPECIAL = '<span class="sr-only visually-hidden">original price</span><s><span class="specialPrice">%s</span></s> <span class="sr-only visually-hidden">sale price</span><span class="text-danger productPrice">%s</span>';
 const IS_PRODUCT_BUTTON_BUY = '<i class="fas fa-shopping-cart" title="Add To Cart"></i>';
 const IS_PRODUCT_BUTTON_VIEW = '<i class="fas fa-eye" title="View Product"></i>';
+
+const IS_PRODUCT_BUTTON_OPTIONS = '<i class="fas fa-cogs me-2" title="Choose Options"></i>Options';
