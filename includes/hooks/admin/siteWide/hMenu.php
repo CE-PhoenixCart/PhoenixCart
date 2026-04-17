044259f5127b6aa27e6754b3ecee9ac0c9de1fa8
gburton <gb8231059@gmail.com>
2026-04-17 14:21:31 +0100

    Update hMenu.php
    To allow Links, Buttons etc as well as Dropdowns

--- includes/hooks/admin/siteWide/hMenu.php
+++ includes/hooks/admin/siteWide/hMenu.php
@@ -62,7 +62,9 @@ class hook_admin_siteWide_hMenu {
       });
 
       foreach ( $cl_box_groups as &$group ) {
-        usort($group['apps'], 'hook_admin_siteWide_hMenu::sort_box_links');
+        if (!empty($group['apps']) && is_array($group['apps'])) {
+          usort($group['apps'], 'hook_admin_siteWide_hMenu::sort_box_links');
+        }
       }
 
       $n = 1;
@@ -72,34 +74,64 @@ class hook_admin_siteWide_hMenu {
         $item = '';
         
         $area = $groups['nav'] ?? 'start';
+        $type = $groups['type'] ?? 'dropdown';
 
-        $item .= '<li class="nav-item dropdown">';
-          $item .= '<a class="nav-link dropdown-toggle" href="#" id="navbar_' . $n . '" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
-            $item .= $groups['heading'];
-          $item .= '</a>';
+        switch ($type) {
+          case 'link':
+            $app = $groups['apps'][0] ?? null;
 
-          $align = ($area === 'end') ? ' dropdown-menu-end' : '';
-          $item .= '<ul class="dropdown-menu' . $align . '" aria-labelledby="navbar_' . $n . '">';
+            if (!empty($app['link'])) {
+              $class = $app['class'] ?? 'dropdown-item';
+              $attrs = '';
 
-          foreach ($groups['apps'] as $app) {
-            $item .= '<li><a class="dropdown-item" href="' . $app['link'] . '">' . $app['title'] . '</a></li>';
+              if (!empty($app['data']) && is_array($app['data'])) {
+                foreach ($app['data'] as $k => $v) {
+                  $attrs .= ' ' . $k . '="' . htmlspecialchars($v) . '"';
+                }
+              }
+
+              $item .= '<li class="nav-item d-flex align-items-center">';
+                $item .= '<a class="' . $class . '" href="' . $app['link'] . '"' . $attrs . '>'
+                  . $app['title'] .
+                '</a>';
+              $item .= '</li>';
+            }
+            break;
+          case 'dropdown':
+            $item .= '<li class="nav-item dropdown">';
+              $item .= '<a class="nav-link dropdown-toggle" href="#" id="navbar_' . $n . '" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
+                $item .= $groups['heading'];
+              $item .= '</a>';
+
+              $align = ($area === 'end') ? ' dropdown-menu-end' : '';
+              $item .= '<ul class="dropdown-menu' . $align . '" aria-labelledby="navbar_' . $n . '">';
+
+              foreach ($groups['apps'] as $app) {
+                $item .= '<li><a class="dropdown-item" href="' . $app['link'] . '">' . $app['title'] . '</a></li>';
+              }
+
+              $item .= '</ul>';
+            $item .= '</li>' . PHP_EOL;
+            break;
+
+          default:
+            break;
+        }
+
+        if (!empty($item)) {
+          if ($area === 'start') {
+            $start .= $item;
+          } elseif ($area === 'center') {
+            $center .= $item;
+          } else {
+            $end .= $item;
           }
-
-          $item .= '</ul>';
-        $item .= '</li>' . PHP_EOL;
-
-        if ($area === 'start') {
-          $start .= $item;
-        } elseif ($area === 'center') {
-          $center .= $item;
-        } else {
-          $end .= $item;
         }
 
         $n++;
       }
 
-      $icon = $GLOBALS['Admin']->image('images/CE-Phoenix-30-30.png', [], 'CE Phoenix v' . Versions::get('Phoenix'), 30, 30);
+      $icon = $GLOBALS['Admin']->image('images/CE-Phoenix-30-30.png', [], 'Phoenix v' . Versions::get('Phoenix'), 30, 30);
       
       $output = '';
       
